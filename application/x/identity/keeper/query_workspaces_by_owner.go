e5b59428c6a00a24ccce95a3064fa68fea8e9e98
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-05-29 15:36:38 +0200

    feat(identity): implement WorkspacesByOwner

--- application/x/identity/keeper/query_workspaces_by_owner.go
+++ application/x/identity/keeper/query_workspaces_by_owner.go
@@ -3,7 +3,9 @@ package keeper
 import (
 	"context"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"gitlab.qredo.com/qrdochain/fusionchain/x/identity/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -16,8 +18,28 @@ func (k Keeper) WorkspacesByOwner(goCtx context.Context, req *types.QueryWorkspa
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	workspaceStore := prefix.NewStore(store, types.KeyPrefix(types.WorkspaceKey))
 
-	return &types.QueryWorkspacesResponse{}, nil
+	workspaces, pageRes, err := query.GenericFilteredPaginate(k.cdc, workspaceStore, req.Pagination, func(key []byte, value *types.Workspace) (*types.Workspace, error) {
+		if !value.IsOwner(req.Owner) {
+			return nil, nil
+		}
+		return value, nil
+	}, func() *types.Workspace { return &types.Workspace{} })
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	// extract pointers
+	wss := make([]types.Workspace, 0, len(workspaces))
+	for _, w := range workspaces {
+		wss = append(wss, *w)
+	}
+
+	return &types.QueryWorkspacesResponse{
+		Workspaces: wss,
+		Pagination: pageRes,
+	}, nil
 }

1e5b3cb022f6b785f9b0b20cb043e57f91688c9a
Sasha Duke <sasha.duke@qredo.com>
2023-06-13 19:55:45 +0100

    update errors to be readable by contract query_verify.go

--- application/x/blackbird/keeper/query_verify.go
+++ application/x/blackbird/keeper/query_verify.go
@@ -3,8 +3,10 @@ package keeper
 import (
 	"context"
 	"fmt"
+	// "strconv"
 	"strings"
 
+	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	"gitlab.qredo.com/edmund/blackbird/verifier/golang/simple"
 	"gitlab.qredo.com/qrdochain/fusionchain/x/blackbird/types"
 	"google.golang.org/grpc/codes"
@@ -27,10 +29,11 @@ func (k Keeper) Verify(goCtx context.Context, req *types.QueryVerifyRequest) (*t
 	oracleMap := make(map[string]bool)
 	for _, v := range strings.Split(req.Payload, ",") {
 		oracleMap[strings.Split(v, ":")[0]] = strings.Split(v, ":")[1] == "1"
+		// return nil, wasmvmtypes.UnsupportedRequest{Kind: strings.Split(v, ":")[0] + strconv.FormatBool(strings.Split(v, ":")[1] == "1")}
 	}
 
 	if err := simple.Verify([]byte(req.Policy), nil, nil, nil, oracleMap); err != nil {
-		return nil, VerificationError{Kind: "payload does not meet policy requirements for verification."}
+		return nil, wasmvmtypes.UnsupportedRequest{Kind: "payload does not meet policy requirements for verification."}
 	}
 
 	return &types.QueryVerifyResponse{Result: true}, nil

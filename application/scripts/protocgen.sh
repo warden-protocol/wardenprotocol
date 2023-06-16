8b958b8406246e49399c5a3107ff12dc5462af93
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-06-16 16:35:38 +0200

    fix: use our local patched Docker image

--- application/scripts/protocgen.sh
+++ application/scripts/protocgen.sh
@@ -2,22 +2,6 @@
 
 set -eo pipefail
 
-protoc_gen_gocosmos() {
-  if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the sommelier folder."
-    return 1
-  fi
-
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-}
-
-protoc_gen_doc() {
-  go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
-} 
-
-protoc_gen_gocosmos
-protoc_gen_doc
-
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   buf protoc \
@@ -41,4 +25,4 @@ $(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
 
 # move proto files to the right places
 cp -r gitlab.qredo.com/qrdochain/fusionchain/* ./
-rm -rf github.com
+rm -rf gitlab.qredo.com/

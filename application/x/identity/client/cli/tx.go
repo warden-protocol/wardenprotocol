a785fd191be3fdb02d37e2bd2184ac515b973deb
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-06-26 13:01:56 +0200

    fix(identity): change Add/RemoveOwner to produce Actions

--- application/x/identity/client/cli/tx.go
+++ application/x/identity/client/cli/tx.go
@@ -33,6 +33,7 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(CmdNewWorkspace())
 	cmd.AddCommand(CmdAddWorkspaceOwner())
 	cmd.AddCommand(CmdRemoveWorkspaceOwner())
+	cmd.AddCommand(CmdApproveAction())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
--- application/x/identity/keeper/msg_server_remove_workspace_owner.go
+++ application/x/identity/keeper/msg_server_remove_workspace_owner.go
@@ -5,32 +5,50 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"gitlab.qredo.com/qrdochain/fusionchain/policy"
 	"gitlab.qredo.com/qrdochain/fusionchain/x/identity/types"
 )
 
 func (k msgServer) RemoveWorkspaceOwner(goCtx context.Context, msg *types.MsgRemoveWorkspaceOwner) (*types.MsgRemoveWorkspaceOwnerResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	ws, found := k.GetWorkspace(ctx, msg.WorkspaceId)
-	if !found {
-		return nil, fmt.Errorf("workspace not found")
+	act, err := addAction(k, ctx, msg, msg.Creator)
+	if err != nil {
+		return nil, err
 	}
-
-	if !allowedToRemoveOwner(ws, msg.Creator) {
-		return nil, fmt.Errorf("sender is not allowed to perform this action")
-	}
-
-	if !ws.IsOwner(msg.Owner) {
-		return nil, fmt.Errorf("owner does not exist")
-	}
-
-	ws.RemoveOwner(msg.Owner)
-
-	k.SetWorkspace(ctx, ws)
-
-	return &types.MsgRemoveWorkspaceOwnerResponse{}, nil
+	return k.xxxRemoveOwnerAction(ctx, act)
 }
 
-func allowedToRemoveOwner(ws types.Workspace, addr string) bool {
-	return ws.IsOwner(addr)
+func (k msgServer) xxxRemoveOwnerAction(ctx sdk.Context, act *types.Action) (*types.MsgRemoveWorkspaceOwnerResponse, error) {
+	return tryExecuteAction(
+		k,
+		k.cdc,
+		ctx,
+		act,
+		func(ctx sdk.Context, msg *types.MsgRemoveWorkspaceOwner) (policy.Policy, error) {
+			ws, found := k.GetWorkspace(ctx, msg.WorkspaceId)
+			if !found {
+				return nil, fmt.Errorf("workspace not found")
+			}
+
+			pol := ws.PolicyRemoveOwner()
+			return pol, nil
+		},
+		func(ctx sdk.Context, msg *types.MsgRemoveWorkspaceOwner) (*types.MsgRemoveWorkspaceOwnerResponse, error) {
+			ws, found := k.GetWorkspace(ctx, msg.WorkspaceId)
+			if !found {
+				return nil, fmt.Errorf("workspace not found")
+			}
+
+			if !ws.IsOwner(msg.Owner) {
+				return nil, fmt.Errorf("owner does not exist")
+			}
+
+			ws.RemoveOwner(msg.Owner)
+
+			k.SetWorkspace(ctx, ws)
+
+			return &types.MsgRemoveWorkspaceOwnerResponse{}, nil
+		},
+	)
 }

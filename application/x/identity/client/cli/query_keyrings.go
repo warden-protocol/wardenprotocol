e6754c235856ff3d9b597c334e022428ce1f2296
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-07-11 12:26:59 +0200

    feat(identity): add Keyrings query

---
+++ application/x/identity/client/cli/query_keyrings.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+	"gitlab.qredo.com/qrdochain/fusionchain/x/identity/types"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdKeyrings() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "keyrings",
+		Short: "Query Keyrings",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryKeyringsRequest{}
+
+			res, err := queryClient.Keyrings(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}

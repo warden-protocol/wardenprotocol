0bd4afd01b250962f61b5383bf2c646a579d2969
Sasha Duke <sasha.duke@qredo.com>
2023-07-21 15:41:11 +0100

    change rootcmd name

--- application/cmd/ethermintd/root.go
+++ application/cmd/ethermintd/root.go
@@ -61,7 +61,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   "fusiond",
-		Short: "Ethermint Daemon",
+		Short: "Fusion Daemon",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())

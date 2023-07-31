b5459d9d53451ff7829e8603050be153aaedb9c0
Antonio Pitasi <antonio.pitasi@qredo.com>
2023-07-31 15:49:26 +0200

    chore: enable linting for relayer-eth and go-client

---
+++ relayer-eth/golint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# golanci needs to be in the same dir as the go application
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+go mod tidy
+
+golangci-lint run \
+    --config ../.golangci.yml \
+    --timeout 10m

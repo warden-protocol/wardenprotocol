db2ebbabc794f5a71d02d461c636109c421b45fc
Iris Garcia <iris.garcia.desebastian@gmail.com>
2023-11-30 14:13:37 +0100

    feat(docker): small lint changes (#150)

--- docker/docker-build-faucet.sh
+++ docker/docker-build-faucet.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,9 +6,9 @@ commit_hash=$(git rev-parse HEAD)
 commit_hash_short=$(git rev-parse --short HEAD)
 
 docker build \
-       --build-arg BUILD_DATE="$(git show -s --format=%ci $commit_hash)"\
+       --build-arg BUILD_DATE="$(git show -s --format=%ci "$commit_hash")"\
        --build-arg SERVICE=faucet \
-       --build-arg GIT_SHA=$commit_hash \
-       -t ${ECR}faucet:latest  \
-       -t ${ECR}faucet:$commit_hash_short  \
+       --build-arg GIT_SHA="$commit_hash" \
+       -t "${ECR}"faucet:latest  \
+       -t "${ECR}"faucet:"$commit_hash_short"  \
        -f Dockerfile-faucet ..
--- docker/docker-build-package.sh
+++ docker/docker-build-package.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,9 +6,9 @@ commit_hash=$(git rev-parse HEAD)
 commit_hash_short=$(git rev-parse --short HEAD)
 
 docker build \
-       --build-arg BUILD_DATE="$(git show -s --format=%ci $commit_hash)"\
+       --build-arg BUILD_DATE="$(git show -s --format=%ci "$commit_hash")"\
        --build-arg SERVICE=fusion-stack \
-       --build-arg GIT_SHA=$commit_hash \
-       -t ${ECR}fusion-stack:latest  \
-       -t ${ECR}fusion-stack:$commit_hash_short  \
+       --build-arg GIT_SHA="$commit_hash" \
+       -t "${ECR}"fusion-stack:latest  \
+       -t "${ECR}"fusion-stack:"$commit_hash_short"  \
        -f Dockerfile-package ..

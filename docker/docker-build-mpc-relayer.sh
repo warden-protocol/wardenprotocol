3854fb3c41cea0bbb4ebd5386ff0b21665d68a4e
Alex Mackay <52156545+ATMackay@users.noreply.github.com>
2023-12-11 13:48:42 +0000

    fix(docker): support Mac OS and Linux using build args (#169)
    * fix(docker): support Mac OS and Linux using build args
    
    * declare arg

--- docker/docker-build-mpc-relayer.sh
+++ docker/docker-build-mpc-relayer.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+# Version control
 commit_hash=$(git rev-parse HEAD)
 commit_hash_short=$(git rev-parse --short HEAD)
 
+# Set ARCH variable based on the architecture
+architecture=$(uname -m)
+if [ "$architecture" == "x86_64" ]; then
+    ARCH="x86_64" # Linux, Windows
+else
+    ARCH="aarch64" # Mac
+fi
+
 docker build \
+       --build-arg ARCH="$ARCH" \
        --build-arg BUILD_DATE="$(git show -s --format=%ci "$commit_hash")"\
        --build-arg SERVICE=mpc-relayer \
        --build-arg GIT_SHA="$commit_hash" \

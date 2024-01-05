4724ac86daf7d3b665ee46fe0bb879f5946bd8a6
Antonio Pitasi <antonio.pitasi@qredo.com>
2024-01-05 17:39:57 +0100

    fix(web): view overflowing when user had multiple workspaces (#213)

--- web/src/components/workspaces.tsx
+++ web/src/components/workspaces.tsx
@@ -12,31 +12,29 @@ export default function Workspaces({ owner }: { owner: string }) {
   const count = wsQuery.data?.workspaces.length;
 
   return (
-    <div className="flex items-center content-center h-[50vh] place-content-center">
+    <div className="flex items-center justify-center">
       {count && count > 0 ? (
-        <div className="mt-6">
+        <div className="flex flex-col mt-6 gap-4">
           {wsQuery.data?.workspaces.map((workspace) => (
             <Workspace key={workspace.address} workspace={workspace} />
           ))}
         </div>
       ) : (
-        <div>
-          <div className="text-center">
-            <h3 className="mt-2 text-3xl text-gray-900">No workspaces</h3>
-            <p className="mt-1 text-gray-500">Get started by creating a new workspace.</p>
-            <div className="mt-6">
-              <Button
-                type="button"
-                onClick={() => {
-                  broadcast([
-                    new MsgNewWorkspace({ creator: owner }),
-                  ]);
-                }}
-              >
-                <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-                New Workspace
-              </Button>
-            </div>
+        <div className="text-center">
+          <h3 className="mt-2 text-3xl text-gray-900">No workspaces</h3>
+          <p className="mt-1 text-gray-500">Get started by creating a new workspace.</p>
+          <div className="mt-6">
+            <Button
+              type="button"
+              onClick={() => {
+                broadcast([
+                  new MsgNewWorkspace({ creator: owner }),
+                ]);
+              }}
+            >
+              <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+              New Workspace
+            </Button>
           </div>
         </div>
       )}

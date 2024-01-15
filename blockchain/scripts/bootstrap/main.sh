32381d6c2a0ec7e155cfcef91f205019fb64fef3
Iris Garcia <iris.garcia.desebastian@gmail.com>
2024-01-15 13:23:19 +0100

    fix(blockchain): use test keyring backend in bootstrap script (#220)

--- blockchain/scripts/bootstrap/main.sh
+++ blockchain/scripts/bootstrap/main.sh
@@ -18,17 +18,15 @@ usage() {
     echo "Usage: $SCRIPT"
     echo ""
     echo "-n <number>  -- number of validators"
-    echo "-k <string>  -- keyring backend (os|file|test|pass|kwallet|memory)"
     echo "-c <string>  -- chain ID"
     echo "-b <string>  -- amount of coins to bond"
     exit 1
 }
 
-while getopts "h?n:k:c:" args; do
+while getopts "h?n:c:" args; do
     case $args in
         h|\?) usage;;
         n ) N_VALIDATORS=${OPTARG};;
-        k ) KEYRING_BACKEND=${OPTARG};;
         c ) CHAIN_ID=${OPTARG};;
         b ) BOND_COINS=${OPTARG};;
     esac
@@ -54,30 +52,13 @@ $FUSIOND_BIN init bootstrap --chain-id "$CHAIN_ID" --home "$ARTIFACTS_DIR" 1>/de
 # Bootstrap validator/s
 for i in $(seq "$N_VALIDATORS"); do
     echo "Bootstraping validator $i"
-    if [ "$KEYRING_BACKEND" != "test" ]; then
-        read -p "Enter passphrase for validator $i: " -s pass
-        # WORKAROUND: First key asks two times the passphrase
-        if [ "$i" -eq 1 ]; then
-            (echo "$pass"; echo "$pass") | $FUSIOND_BIN keys add "validator_operator_$i" --home "$ARTIFACTS_DIR" 2>&1 | \
-                tail -n2 | head -n1 > "$ARTIFACTS_DIR/mnemonic_$i"
-        else
-            echo "$pass" | $FUSIOND_BIN keys add "validator_operator_$i" --home "$ARTIFACTS_DIR" 2>&1 | \
-                tail -n2 | head -n1 > "$ARTIFACTS_DIR/mnemonic_$i"
-        fi
-        echo "$pass" | $FUSIOND_BIN add-genesis-account "validator_operator_$i" 100000000000000000000000000nQRDO \
-            --keyring-backend "$KEYRING_BACKEND" --home "$ARTIFACTS_DIR"
-        echo "$pass" | $FUSIOND_BIN gentx "validator_operator_$i" "$BOND_COINS" \
-            --keyring-backend "$KEYRING_BACKEND" --chain-id "$CHAIN_ID" --home "$ARTIFACTS_DIR" \
-            --moniker "validator-$i" --note "validator-$i"
-    else
-        $FUSIOND_BIN keys add "validator_operator_$i" --home "$ARTIFACTS_DIR" 2>&1 | \
-            tail -n2 | head -n1 > "$ARTIFACTS_DIR/mnemonic_$i"
-        $FUSIOND_BIN add-genesis-account "validator_operator_$i" 100000000000000000000000000nQRDO \
-            --keyring-backend "$KEYRING_BACKEND" --home "$ARTIFACTS_DIR"
-        $FUSIOND_BIN gentx "validator_operator_$i" "$BOND_COINS" --keyring-backend "$KEYRING_BACKEND" \
-            --chain-id "$CHAIN_ID" --home "$ARTIFACTS_DIR" --moniker "validator-$i" --note "validator-$i"
-    fi
-
+    $FUSIOND_BIN keys add "validator_operator_$i" --home "$ARTIFACTS_DIR" 2>&1 | \
+        tail -n2 | head -n1 > "$ARTIFACTS_DIR/mnemonic_$i"
+    $FUSIOND_BIN add-genesis-account "validator_operator_$i" 100000000000000000000000000nQRDO \
+        --keyring-backend "$KEYRING_BACKEND" --home "$ARTIFACTS_DIR"
+    $FUSIOND_BIN gentx "validator_operator_$i" "$BOND_COINS" \
+        --keyring-backend "$KEYRING_BACKEND" --chain-id "$CHAIN_ID" --home "$ARTIFACTS_DIR" \
+        --moniker "validator-$i" --note "validator-$i"
     mv "$ARTIFACTS_DIR/config/node_key.json" "$ARTIFACTS_DIR/validator_${i}_node_key.json"
     mv "$ARTIFACTS_DIR/config/priv_validator_key.json" "$ARTIFACTS_DIR/validator_${i}_priv_validator_key.json"
 done

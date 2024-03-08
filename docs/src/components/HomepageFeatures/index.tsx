83745a5a9280c82da90b87ff9971707a5a0bcc38
Jon Heywood <jon@qrdo.org>
2024-03-08 11:27:23 +0100

    docs: another test for vercel integration

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -10,17 +10,6 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
 	{
-		title: "Focus on What Matters",
-		Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
-		description: (
-			<>
-				Docusaurus lets you focus on your docs, and we&apos;ll do the
-				chores. Go ahead and move your docs into the <code>docs</code>{" "}
-				directory.
-			</>
-		),
-	},
-	{
 		title: "Powered by React",
 		Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
 		description: (

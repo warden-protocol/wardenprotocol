35ccc90e4c645401929535a1633195b6d99d4ebb
r0t0r-r0t0r <4959423+r0t0r-r0t0r@users.noreply.github.com>
2024-04-11 10:13:40 +0300

    feat(x/wasm): integrate CosmWasm (#156)
    * feat(wardend): add wasm module support
    
    * fix(wardend): codereview fixes
    
    ---------
    
    Co-authored-by: Pavel Afanasyev <pavel.afanasyev@equilibrium.io>

---
+++ warden/app/wasm.go
@@ -0,0 +1,16 @@
+package app
+
+// AllCapabilities returns all capabilities available with the current wasmvm
+// See https://github.com/CosmWasm/cosmwasm/blob/main/docs/CAPABILITIES-BUILT-IN.md
+// This functionality is going to be moved upstream: https://github.com/CosmWasm/wasmvm/issues/425
+func AllCapabilities() []string {
+	return []string{
+		"iterator",
+		"staking",
+		"stargate",
+		"cosmwasm_1_1",
+		"cosmwasm_1_2",
+		"cosmwasm_1_3",
+		"cosmwasm_1_4",
+	}
+}

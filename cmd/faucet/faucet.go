bef449cb055f27539fbc14c5e50aca5f7a3974dd
Antonio Pitasi <antonio@pitasi.dev>
2024-04-23 18:00:57 +0200

    feat(faucet): use X-Real-Ip header (#218)

--- cmd/faucet/faucet.go
+++ cmd/faucet/faucet.go
@@ -366,8 +366,6 @@ curl --json '{"address":"$YOUR_ADDRESS"}' \
 			return
 		}
 
-		log.Printf("request from %s for %s. Headers: %s", ip, req.Address, r.Header)
-
 		err = c.Send(r.Context(), req.Address, ip)
 		if errors.Is(err, ErrRateLimited) {
 			log.Printf("error: %v", err)
@@ -390,7 +388,7 @@ curl --json '{"address":"$YOUR_ADDRESS"}' \
 }
 
 func getIP(r *http.Request) (string, error) {
-	forwardedFor := r.Header.Get("X-Forwarded-For")
+	forwardedFor := r.Header.Get("X-Real-Ip")
 	if forwardedFor != "" {
 		hops := strings.SplitN(forwardedFor, ",", 2)
 		client := strings.TrimSpace(hops[0])

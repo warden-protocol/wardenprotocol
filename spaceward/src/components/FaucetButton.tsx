9f7e078bdeff008a3227d65ef900d521daa554a9
Antonio Pitasi <antonio@pitasi.dev>
2024-05-28 17:28:41 +0200

    fix(spaceward): redirect browser to faucet v2

--- spaceward/src/components/FaucetButton.tsx
+++ spaceward/src/components/FaucetButton.tsx
@@ -1,81 +1,23 @@
-import { useState } from "react";
 import { env } from "@/env";
 import { useAddressContext } from "@/hooks/useAddressContext";
 import { Button } from "@/components/ui/button";
-import Plausible from "plausible-tracker";
-import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
-import { useToast } from "./ui/use-toast";
-
-async function getFaucetTokens(addr: string) {
-	const res = await fetch(env.faucetURL, {
-		method: "POST",
-		body: JSON.stringify({ address: addr }),
-	});
-	if (!res.ok) {
-		const data = await res.json();
-		throw new Error(data.message);
-	}
-}
+import { PlusCircleIcon } from "lucide-react";
 
 function FaucetButton() {
-	const [loading, setLoading] = useState(false);
 	const { address } = useAddressContext();
-	const { toast } = useToast();
-
-	const { trackEvent } = Plausible();
-
-	const getTokens = async () => {
-		setLoading(true);
-		const { id, update } = toast({
-			title: "Requesting tokens",
-			duration: 5000,
-		});
-		try {
-			await getFaucetTokens(address);
-			update({
-				id,
-				title: "Tokens requested",
-				description:
-					"Please wait a few seconds for the tokens to arrive.",
-				duration: 5000,
-			});
-			trackEvent("Get WARD");
-		} catch (err) {
-			update({
-				id,
-				title: "Error getting tokens",
-				description:
-					"If you already requested tokens recently, please wait a few hours before trying again.",
-				duration: 5000,
-			});
-		} finally {
-			setTimeout(() => {
-				setLoading(false);
-			}, 5000);
-		}
-	};
+	const u = new URL(env.faucetURL);
+	u.searchParams.set("addr", address);
 
 	return (
 		<Button
-			disabled={loading}
-			onClick={() => getTokens()}
 			className="w-full h-12 gap-2"
 			size={"sm"}
+			asChild
 		>
-			{loading ? (
-				<>
-					<RefreshCwIcon
-						strokeWidth={1}
-						className="h-6 w-6 animate-spin"
-					/>
-					Please wait
-				</>
-			) : (
-				<>
-					<PlusCircleIcon strokeWidth={1} className="h-6 w-6" />
-					Get WARD
-				</>
-			)}
+			<a href={u.toString()} target="_blank">
+				<PlusCircleIcon strokeWidth={1} className="h-6 w-6" />
+				Get WARD
+			</a>
 		</Button>
 	);
 }

437e1013f0bfb97f7298f6daf8cfb0a461d55f40
Antonio Pitasi <antonio@pitasi.dev>
2024-05-31 16:46:11 +0200

    chore(x/warden): cleanup old or unused code
    I took the chance to rename "creator" into "authority" since it makes
    more sense, as it will be populated with the module account address of
    x/intent.
    
    I removed broken code for registering Action handlers, which no longer
    exist.
    
    I removed the `btl` field, present in many messages, as it now it's
    specified only once in MsgNewAction and will be resposability of
    x/intent to handle it.

--- warden/x/warden/keeper/msg_server.go
+++ warden/x/warden/keeper/msg_server.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	intenttypes "github.com/warden-protocol/wardenprotocol/warden/x/intent/types"
 	types "github.com/warden-protocol/wardenprotocol/warden/x/warden/types/v1beta2"
 )
 
@@ -12,25 +11,7 @@ type msgServer struct {
 // NewMsgServerImpl returns an implementation of the MsgServer interface
 // for the provided Keeper.
 func NewMsgServerImpl(keeper Keeper) types.MsgServer {
-	msgServer := &msgServer{Keeper: keeper}
-
-	var handlers = []struct {
-		actionType    string
-		actionHandler intenttypes.ActionHandler
-	}{
-		{"/warden.warden.v1beta2.MsgAddSpaceOwner", msgServer.AddOwnerActionHandler},
-		{"/warden.warden.v1beta2.MsgNewKeyRequest", msgServer.NewKeyRequestActionHandler},
-		{"/warden.warden.v1beta2.MsgNewSignatureRequest", msgServer.NewSignatureRequestActionHandler},
-		{"/warden.warden.v1beta2.MsgRemoveSpaceOwner", msgServer.RemoveOwnerActionHandler},
-		{"/warden.warden.v1beta2.MsgUpdateKey", msgServer.UpdateKeyActionHandler},
-		{"/warden.warden.v1beta2.MsgUpdateSpace", msgServer.UpdateSpaceActionHandler},
-	}
-
-	for _, handler := range handlers {
-		msgServer.intentKeeper.RegisterActionHandler(handler.actionType, handler.actionHandler)
-	}
-
-	return msgServer
+	return &msgServer{Keeper: keeper}
 }
 
 var _ types.MsgServer = msgServer{}

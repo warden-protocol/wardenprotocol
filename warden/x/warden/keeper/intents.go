ecdfe1f7f19651e750dbd7b1978037045cb7beae
Antonio Pitasi <antonio@pitasi.dev>
2024-05-31 16:38:48 +0200

    fix(x/warden): execute RemoveSpaceOwner from Actions

--- warden/x/warden/keeper/intents.go
+++ warden/x/warden/keeper/intents.go
@@ -9,6 +9,7 @@ import (
 func (k Keeper) RegisterIntents(reg *intenttypes.IntentsRegistry) {
 	intenttypes.Register(reg, k.addSpaceOwnerIntent)
 	intenttypes.Register(reg, k.newKeyRequestIntent)
+	intenttypes.Register(reg, k.removeSpaceOwnerIntent)
 }
 
 func (k Keeper) addSpaceOwnerIntent(ctx context.Context, msg *v1beta2.MsgAddSpaceOwner) (intenttypes.Intent, error) {
@@ -21,6 +22,19 @@ func (k Keeper) addSpaceOwnerIntent(ctx context.Context, msg *v1beta2.MsgAddSpac
 		return k.intentKeeper.GetIntent(ctx, space.AdminIntentId)
 	} else {
 		return space.IntentAddOwner(), nil
+	}
+}
+
+func (k Keeper) removeSpaceOwnerIntent(ctx context.Context, msg *v1beta2.MsgRemoveSpaceOwner) (intenttypes.Intent, error) {
+	space, err := k.SpacesKeeper.Get(ctx, msg.SpaceId)
+	if err != nil {
+		return intenttypes.Intent{}, err
+	}
+
+	if space.AdminIntentId > 0 {
+		return k.intentKeeper.GetIntent(ctx, space.AdminIntentId)
+	} else {
+		return space.IntentRemoveOwner(), nil
 	}
 }
 
--- warden/x/warden/keeper/msg_server_remove_space_owner.go
+++ warden/x/warden/keeper/msg_server_remove_space_owner.go
@@ -4,43 +4,11 @@ import (
 	"context"
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/gogoproto/proto"
-	intenttypes "github.com/warden-protocol/wardenprotocol/warden/x/intent/types"
 	types "github.com/warden-protocol/wardenprotocol/warden/x/warden/types/v1beta2"
 )
 
-func (k msgServer) RemoveSpaceOwner(goCtx context.Context, msg *types.MsgRemoveSpaceOwner) (*intenttypes.MsgActionCreated, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-	space, err := k.SpacesKeeper.Get(ctx, msg.SpaceId)
-	if err != nil {
-		return nil, err
-	}
-
-	intent, err := k.removeSpaceOwnerIntent(ctx, space)
-	if err != nil {
-		return nil, err
-	}
-
-	act, err := k.intentKeeper.AddAction(ctx, msg.Creator, msg, intent, msg.Btl)
-	if err != nil {
-		return nil, err
-	}
-
-	return &intenttypes.MsgActionCreated{Action: act}, nil
-}
-
-func (k msgServer) removeSpaceOwnerIntent(ctx sdk.Context, space types.Space) (intenttypes.Intent, error) {
-	if space.AdminIntentId > 0 {
-		return k.intentKeeper.GetIntent(ctx, space.AdminIntentId)
-	} else {
-		return space.IntentRemoveOwner(), nil
-	}
-}
-
-func (k msgServer) RemoveOwnerActionHandler(ctx context.Context, act intenttypes.Action) (proto.Message, error) {
-	msg, err := intenttypes.GetActionMessage[*types.MsgRemoveSpaceOwner](k.cdc, act)
-	if err != nil {
+func (k msgServer) RemoveSpaceOwner(ctx context.Context, msg *types.MsgRemoveSpaceOwner) (*types.MsgRemoveSpaceOwnerResponse, error) {
+	if err := k.assertIntentAuthority(msg.Authority); err != nil {
 		return nil, err
 	}
 

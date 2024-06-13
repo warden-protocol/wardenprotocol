10a6fb9ede3b513b17ef1cae387341fc4367a2c1
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-06-13 13:16:30 +0300

    Add a comment

--- warden/x/gmp/types/params.go
+++ warden/x/gmp/types/params.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// axelar1zl3rxpp70lmte2xr6c4lgske2fyuj3hupcsvcd testnet fee recipient
+// axelar1aythygn6z5thymj6tmzfwekzh05ewg3l7d6y89 mainnet fee recipient
 var (
 	DefaultGMPAddress   = "axelar1dv4u5k73pzqrxlzujxg3qp8kvc3pje7jtdvu72npnt5zhq05ejcsn5qme5"
 	DefaultChannel      = "channel-1"

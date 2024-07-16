60b5bb61564bd94197543681fb07fd11c793c079
backsapc <backsapce@hotmail.com>
2024-07-16 12:16:48 +0300

    #466 Manual Check Action (#510)
    * Added MSgCheckAction
    
    * generated files from proto
    
    * Updated CheckActionResponse
    
    * generated files from proto
    
    * Added CheckAction method
    
    * Added CheckAction method to server
    
    * generated files from proto
    
    * Changelog update

---
+++ warden/x/act/keeper/msg_server_check_action.go
@@ -0,0 +1,37 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	types "github.com/warden-protocol/wardenprotocol/warden/x/act/types/v1beta1"
+)
+
+func (k msgServer) CheckAction(goCtx context.Context, msg *types.MsgCheckAction) (*types.MsgCheckActionResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	act, err := k.ActionKeeper.Get(ctx, msg.ActionId)
+	if err != nil {
+		return nil, err
+	}
+
+	if act.Status != types.ActionStatus_ACTION_STATUS_PENDING {
+		return nil, fmt.Errorf("action not pending %s", act.Status.String())
+	}
+	if act.TimeoutHeight > 0 && act.TimeoutHeight < uint64(ctx.BlockHeight()) {
+		if err := act.SetStatus(ctx, types.ActionStatus_ACTION_STATUS_TIMEOUT); err != nil {
+			return nil, err
+		}
+		err := k.ActionKeeper.Set(ctx, act)
+		if err != nil {
+			return nil, err
+		}
+
+		return &types.MsgCheckActionResponse{Status: act.Status.String()}, nil
+	}
+
+	if err := k.TryExecuteAction(ctx, &act); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgCheckActionResponse{Status: act.Status.String()}, nil
+}

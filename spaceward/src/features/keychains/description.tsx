b189a65797ed569eda67d1fe54b281b22912bae9
Jon Heywood <94459819+jjheywood@users.noreply.github.com>
2024-08-01 12:15:53 +0200

    add keychain descriptions

--- spaceward/src/features/keychains/description.tsx
+++ spaceward/src/features/keychains/description.tsx
@@ -3,15 +3,44 @@ import { Fragment } from "react";
 const descriptions = [
 	{
 		key: "WardenKMS",
+		description: <Fragment>WardenKMS</Fragment>,
+		title: "WardenKMS",
+		verified: true,
+		link: "#",
+	},
+	{
+		key: "OCP KMS",
+		description: <Fragment>OCP KMS</Fragment>,
+		title: "OCP KMS",
+		verified: true,
+		link: "#",
+	},
+	{
+		key: "Open Custody Protocol (Fordefi MPC)",
+		description: <Fragment>Open Custody Protocol (Fordefi MPC)</Fragment>,
+		title: "Open Custody Protocol (Fordefi MPC)",
+		verified: true,
+		link: "#",
+	},
+	{
+		key: "Bushinode Keychain",
+		description: <Fragment>Bushinode Keychain</Fragment>,
+		title: "Bushinode Keychain",
+		verified: false,
+		link: "#",
+	},
+	{
+		key: "Silence Laboratories",
 		description: (
 			<Fragment>
-				A modular custody primitive to expand the original vision from
-				Qredo by connecting multiple next generation custodial and key
-				management solutions to builders. Powered by the OPEN token
-				(previously known as QRDO) &#183; opencustody.org
+				One of the fastest MPC-based TSS for usable, secure, and truly
+				decentralized support for digital wallets, exchanges and
+				institutional asset enterprises. Silent Shard performs
+				distributed signatures in less than 10 ms and key generation in
+				less than 10s ms for 3 party settings.
 			</Fragment>
 		),
-		title: "OCP KMS",
+		title: "Silence Laboratories",
 		verified: true,
 		link: "#",
 	},

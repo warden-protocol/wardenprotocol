0a15d0c3b5b3dcbb0f258884c151eabda62063af
Jon Heywood <94459819+jjheywood@users.noreply.github.com>
2024-08-01 13:26:36 +0200

    fix(spaceward): add keychain descriptions (#604)
    * add keychain descriptions
    
    * updated descriptions
    
    * added disabled field to keychains
    
    * add key description
    
    ---------
    
    Co-authored-by: Alex <alex.d.nax@gmail.com>

--- spaceward/src/features/keychains/description.tsx
+++ spaceward/src/features/keychains/description.tsx
@@ -3,16 +3,66 @@ import { Fragment } from "react";
 const descriptions = [
 	{
 		key: "WardenKMS",
+		description: <Fragment>WardenKMS</Fragment>,
+		title: "WardenKMS",
+		verified: true,
+		disabled: false,
+		link: "#",
+	},
+	{
+		key: "OCP KMS",
 		description: (
 			<Fragment>
-				A modular custody primitive to expand the original vision from
-				Qredo by connecting multiple next generation custodial and key
-				management solutions to builders. Powered by the OPEN token
-				(previously known as QRDO) &#183; opencustody.org
+				Introducing OCP KMS: Your gateway to effortless and robust
+				crypto security! This innovative key management solution
+				streamlines digital asset protection, offering a user-friendly
+				experience that caters to both newcomers and seasoned crypto
+				enthusiasts alike.
 			</Fragment>
 		),
 		title: "OCP KMS",
 		verified: true,
+		disabled: false,
+		link: "#",
+	},
+	{
+		key: "Open Custody Protocol (Fordefi MPC)",
+		description: (
+			<Fragment>
+				Unlock the future of secure digital asset management with Open
+				Custody Protocol's cutting-edge MPC keychain! Powered by
+				Fordefi's advanced wallet tech and trusted execution
+				environments, it's your key to next-level crypto safety and
+				flexibility.
+			</Fragment>
+		),
+		title: "Open Custody Protocol (Fordefi MPC)",
+		verified: true,
+		disabled: false,
+		link: "#",
+	},
+	{
+		key: "Bushinode Keychain",
+		description: <Fragment>Bushinode Keychain</Fragment>,
+		title: "Bushinode Keychain",
+		verified: false,
+		disabled: true,
+		link: "#",
+	},
+	{
+		key: "Silence Laboratories",
+		description: (
+			<Fragment>
+				One of the fastest MPC-based TSS for usable, secure, and truly
+				decentralized support for digital wallets, exchanges and
+				institutional asset enterprises. Silent Shard performs
+				distributed signatures in less than 10 ms and key generation in
+				less than 10s ms for 3 party settings.
+			</Fragment>
+		),
+		title: "Silence Laboratories",
+		verified: true,
+		disabled: false,
 		link: "#",
 	},
 ];

084117d201d430732a7a5910940adddb19214545
Artur Abliazimov <artur.abliazimov@equilibrium.io>
2024-08-26 14:47:27 +0300

    Remove unused project

--- relayers/packages/wormhole/contracts/solana/programs/gmp/src/context/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use initialize::*;
-pub use receive_message::*;
-pub use register_emitter::*;
-pub use send_message::*;
-
-pub mod initialize;
-pub mod receive_message;
-pub mod register_emitter;
-pub mod send_message;
--- relayers/packages/wormhole/contracts/solana/programs/gmp/src/state/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use config::*;
-pub use foreign_emitter::*;
-pub use received::*;
-pub use wormhole_emitter::*;
-
-pub mod config;
-pub mod foreign_emitter;
-pub mod received;
-pub mod wormhole_emitter;

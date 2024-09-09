7817eb10e6d7b2c0a4769c572413e77d3d56adb2
Alex <alex.d.nax@gmail.com>
2024-09-09 18:30:05 +0200

    fixes https://github.com/warden-protocol/wardenprotocol/issues/374 fixes https://github.com/warden-protocol/wardenprotocol/issues/375

--- spaceward/src/features/modals/types.ts
+++ spaceward/src/features/modals/types.ts
@@ -1,6 +1,7 @@
 import type { AddressType } from "@wardenprotocol/wardenjs/codegen/warden/warden/v1beta3/key";
 import type { QueryKeyResponse } from "@wardenprotocol/wardenjs/codegen/warden/warden/v1beta3/query";
 import type { ModalType } from "./state";
+import { ReactNode } from "react";
 
 export interface CreateKeyParams {
 	next?: ModalType;
@@ -18,6 +19,12 @@ export interface TransferParams {
 
 export interface SelectAssetParams {
 	keyResponse?: QueryKeyResponse;
+}
+
+export interface ConfirmParams {
+	content?: ReactNode;
+	onConfirm?: () => Promise<void>;
+	onCancel?: () => Promise<void>;
 }
 
 export type ModalParams<T extends {}> = {

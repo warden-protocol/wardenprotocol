9abd5f757e3710ed9f7b8b94a04f701125346f88
Maks Nabokov <max.nab@equilibrium.io>
2024-09-12 12:30:09 +0300

    chore: blank lines

--- warden/x/warden/keeper/rules.go
+++ warden/x/warden/keeper/rules.go
@@ -64,13 +64,11 @@ func (k Keeper) newKeyRequestRule(ctx context.Context, msg *v1beta3.MsgNewKeyReq
 	}
 
 	approveRule, err := k.getApproveNewKeyRequestRule(ctx, space)
-
 	if err != nil {
 		return acttypes.Rule{}, acttypes.Rule{}, err
 	}
 
 	rejectRule, err := k.getRejectNewKeyRequestRule(ctx, space)
-
 	if err != nil {
 		return acttypes.Rule{}, acttypes.Rule{}, err
 	}

350bca1343c2ebe1650b4ba9a9fc8f500afdec3c
Maks Nabokov <max.nab@equilibrium.io>
2024-09-20 17:07:16 +0300

    addKeychainWriter, fulfilKeyRequest

--- precompiles/warden/IWarden.sol
+++ precompiles/warden/IWarden.sol
@@ -17,15 +17,61 @@ interface IWarden {
     /// @dev Defines a method for adding a new admin to keychain.
     /// @param keychainId The keychain id
     /// @param newAdmin The new admin's address
-    /// @return adminsCount The new count of admins
+    /// @return success If execution was successful
     function addKeychainAdmin(
         uint64 keychainId,
         address newAdmin
-    ) external returns (uint64 adminsCount);
+    ) external returns (bool success);
+
+    /// @dev Defines a method for adding a new writer to keychain.
+    /// @param keychainId The keychain id
+    /// @param newWriter The new writer's address
+    /// @return success If execution was successful
+    function addKeychainWriter(
+        uint64 keychainId,
+        address newWriter
+    ) external returns (bool success);
+
+    /// @dev Defines a method to fulfil a key request.
+    /// @param requestId The request id
+    /// @param pubKey The new created public key
+    /// @return success If execution was successful
+    function fulfilKeyRequest(
+        uint64 requestId,
+        bytes calldata pubKey
+    ) external returns (bool success);
+
+    /// @dev Defines a method to reject a key request.
+    /// @param requestId The request id
+    /// @param rejectReason The reject reason
+    /// @return success If execution was successful
+    function rejectKeyRequest(
+        uint64 requestId,
+        string calldata rejectReason
+    ) external returns (bool success);
 
     /// @dev AddKeychainAdmin defines an Event emitted when add a new admin to keychain.
     /// @param NewAdmin The address of the admin
     /// @param id The keychain id
     /// @param adminsCount The new count of admins
     event AddKeychainAdmin(address indexed NewAdmin, uint64 id, uint64 adminsCount);
+
+    /// @dev AddKeychainWriter defines an Event emitted when add a new writer to keychain.
+    /// @param NewWriter The address of the admin
+    /// @param id The keychain id
+    /// @param adminsCount The new count of admins
+    event AddKeychainWriter(address indexed NewWriter, uint64 id, uint64 adminsCount);
+
+    /// @dev NewKey defines an Event emitted when a key request fulfiled.
+    /// @param id The key id
+    /// @param key_type The key type
+    /// @param space_id The space id
+    /// @param keychain_id The keychain id
+    /// @param approve_template_id The approve template id
+    /// @param reject_template_id The reject template id
+    event NewKey(uint64 id, int32 key_type, uint64 space_id, uint64 keychain_id, uint64 approve_template_id, uint64 reject_template_id);
+
+    /// @dev RejectKeyRequest defines an Event emitted when a key request rejected.
+    /// @param id The request id
+    event RejectKeyRequest(uint64 id);
 }
--- precompiles/warden/events.go
+++ precompiles/warden/events.go
@@ -2,8 +2,10 @@ package warden
 
 import (
 	"bytes"
+	"fmt"
 	"math/big"
 	"reflect"
+	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
@@ -15,9 +17,15 @@ import (
 const (
 	// EventTypeAddKeychainAdmin defines the event type for the x/warden AddKeychainAdmin transaction.
 	EventTypeAddKeychainAdmin = "AddKeychainAdmin"
+	// EventTypeAddKeychainWriter defines the event type for the x/warden AddKeychainWriter transaction.
+	EventTypeAddKeychainWriter = "AddKeychainWriter"
+	// EventTypeNewKey defines the event type for the fulfil branch of x/warden FulfilKeyRequest transaction.
+	EventTypeNewKey = "NewKey"
+	// EventRejectKeyRequest defines the event type for the reject branch of x/warden FulfilKeyRequest transaction.
+	EventRejectKeyRequest = "RejectKeyRequest"
 )
 
-func (p Precompile) EmitAddKeychainAdminEvent(ctx sdk.Context, stateDB vm.StateDB, keychainId uint64, adminAddress common.Address, adminsCount uint64) error {
+func (p Precompile) EmitAddKeychainAdminEvent(ctx sdk.Context, stateDB vm.StateDB, adminAddress common.Address) error {
 	// Prepare the event topics
 	event := p.ABI.Events[EventTypeAddKeychainAdmin]
 
@@ -25,15 +33,177 @@ func (p Precompile) EmitAddKeychainAdminEvent(ctx sdk.Context, stateDB vm.StateD
 	// The first topic is always the signature of the event.
 	topics[0] = event.ID
 
+	sdkEvents := ctx.EventManager().Events()
+	eventAddKeychainWriter := sdkEvents[len(sdkEvents)-1]
+	var b bytes.Buffer
+	for _, attr := range eventAddKeychainWriter.Attributes {
+		key := attr.GetKey()
+		val := strings.Trim(attr.GetValue(), "\"")
+		switch key {
+		case "id":
+			keychainId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("AddKeychainAdminEvent: invalid keychain id type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keychainId)))
+		case "admins_count":
+			keychainId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("AddKeychainAdminEvent: invalid admins count type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keychainId)))
+		}
+	}
+
 	var err error
 	topics[1], err = cmn.MakeTopic(adminAddress)
 	if err != nil {
 		return err
 	}
 
+	stateDB.AddLog(&ethtypes.Log{
+		Address:     p.Address(),
+		Topics:      topics,
+		Data:        b.Bytes(),
+		BlockNumber: uint64(ctx.BlockHeight()),
+	})
+
+	return nil
+}
+
+func (p Precompile) EmitAddKeychainWriterEvent(ctx sdk.Context, stateDB vm.StateDB, writerAddres common.Address) error {
+	// Prepare the event topics
+	event := p.ABI.Events[EventTypeAddKeychainWriter]
+
+	topics := make([]common.Hash, 2)
+	// The first topic is always the signature of the event.
+	topics[0] = event.ID
+
+	sdkEvents := ctx.EventManager().Events()
+	eventAddKeychainWriter := sdkEvents[len(sdkEvents)-1]
 	var b bytes.Buffer
-	b.Write(cmn.PackNum(reflect.ValueOf(new(big.Int).SetUint64(keychainId))))
-	b.Write(cmn.PackNum(reflect.ValueOf(new(big.Int).SetUint64(adminsCount))))
+	for _, attr := range eventAddKeychainWriter.Attributes {
+		key := attr.GetKey()
+		val := strings.Trim(attr.GetValue(), "\"")
+		switch key {
+		case "id":
+			keychainId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("AddKeychainWriterEvent: invalid keychain id type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keychainId)))
+		case "writers_count":
+			keychainId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("AddKeychainWriterEvent: invalid writers count type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keychainId)))
+		}
+	}
+
+	var err error
+	topics[1], err = cmn.MakeTopic(writerAddres)
+	if err != nil {
+		return err
+	}
+
+	stateDB.AddLog(&ethtypes.Log{
+		Address:     p.Address(),
+		Topics:      topics,
+		Data:        b.Bytes(),
+		BlockNumber: uint64(ctx.BlockHeight()),
+	})
+
+	return nil
+}
+
+func (p Precompile) EmitNewKeyEvent(ctx sdk.Context, stateDB vm.StateDB) error {
+	// Prepare the event topics
+	event := p.ABI.Events[EventTypeNewKey]
+
+	topics := make([]common.Hash, 1)
+	// The first topic is always the signature of the event.
+	topics[0] = event.ID
+
+	sdkEvents := ctx.EventManager().Events()
+	eventNewKey := sdkEvents[len(sdkEvents)-1]
+	var b bytes.Buffer
+	for _, attr := range eventNewKey.Attributes {
+		key := attr.GetKey()
+		val := strings.Trim(attr.GetValue(), "\"")
+		switch key {
+		case "id":
+			keyId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("NewKeyEvent: invalid key id type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keyId)))
+		case "key_type":
+			keyType, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("NewKeyEvent: invalid key type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keyType)))
+		case "space_id":
+			spaceId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("NewKeyEvent: invalid space id")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(spaceId)))
+		case "keychain_id":
+			keychainId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("NewKeyEvent: invalid keychain id")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(keychainId)))
+		case "approve_template_id":
+			approveTemplateId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("NewKeyEvent: invalid approve template id")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(approveTemplateId)))
+		case "reject_template_id":
+			rejectTemplateId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("NewKeyEvent: invalid reject template id")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(rejectTemplateId)))
+		}
+	}
+
+	stateDB.AddLog(&ethtypes.Log{
+		Address:     p.Address(),
+		Topics:      topics,
+		Data:        b.Bytes(),
+		BlockNumber: uint64(ctx.BlockHeight()),
+	})
+
+	return nil
+}
+
+func (p Precompile) EmitRejectKeyRequestEvent(ctx sdk.Context, stateDB vm.StateDB) error {
+	// Prepare the event topics
+	event := p.ABI.Events[EventRejectKeyRequest]
+
+	topics := make([]common.Hash, 1)
+	// The first topic is always the signature of the event.
+	topics[0] = event.ID
+
+	sdkEvents := ctx.EventManager().Events()
+	eventNewKey := sdkEvents[len(sdkEvents)-1]
+	var b bytes.Buffer
+	for _, attr := range eventNewKey.Attributes {
+		key := attr.GetKey()
+		val := strings.Trim(attr.GetValue(), "\"")
+		switch key {
+		case "id":
+			requestId, success := new(big.Int).SetString(val, 10)
+			if !success {
+				return fmt.Errorf("RejectKeyRequestEvent: invalid request id type")
+			}
+			b.Write(cmn.PackNum(reflect.ValueOf(requestId)))
+		}
+	}
 
 	stateDB.AddLog(&ethtypes.Log{
 		Address:     p.Address(),

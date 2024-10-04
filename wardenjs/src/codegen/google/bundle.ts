1b2e07426e710fbaf253ec11320e3e101f3d3127
Maks Nabokov <max.nab@equilibrium.io>
2024-10-04 11:12:46 +0300

    update ethermint proto & wardenjs

--- wardenjs/src/codegen/google/bundle.ts
+++ wardenjs/src/codegen/google/bundle.ts
@@ -1,15 +1,15 @@
 //@ts-nocheck
-import * as _81 from "./protobuf/any.js";
-import * as _82 from "./protobuf/descriptor.js";
-import * as _83 from "./protobuf/duration.js";
-import * as _84 from "./protobuf/empty.js";
-import * as _85 from "./protobuf/timestamp.js";
+import * as _82 from "./protobuf/any.js";
+import * as _83 from "./protobuf/descriptor.js";
+import * as _84 from "./protobuf/duration.js";
+import * as _85 from "./protobuf/empty.js";
+import * as _86 from "./protobuf/timestamp.js";
 export namespace google {
   export const protobuf = {
-    ..._81,
     ..._82,
     ..._83,
     ..._84,
-    ..._85
+    ..._85,
+    ..._86
   };
 }
--- wardenjs/src/codegen/slinky/bundle.ts
+++ wardenjs/src/codegen/slinky/bundle.ts
@@ -1,58 +1,41 @@
 //@ts-nocheck
-import * as _86 from "./marketmap/module/v1/module.js";
-import * as _87 from "./marketmap/v1/genesis.js";
-import * as _88 from "./marketmap/v1/market.js";
-import * as _89 from "./marketmap/v1/params.js";
-import * as _90 from "./marketmap/v1/query.js";
-import * as _91 from "./marketmap/v1/tx.js";
-import * as _92 from "./oracle/module/v1/module.js";
-import * as _93 from "./oracle/v1/genesis.js";
-import * as _94 from "./oracle/v1/query.js";
-import * as _95 from "./oracle/v1/tx.js";
-import * as _96 from "./types/v1/currency_pair.js";
-import * as _201 from "./marketmap/v1/tx.amino.js";
-import * as _202 from "./oracle/v1/tx.amino.js";
-import * as _203 from "./marketmap/v1/tx.registry.js";
-import * as _204 from "./oracle/v1/tx.registry.js";
-import * as _205 from "./marketmap/v1/query.lcd.js";
-import * as _206 from "./oracle/v1/query.lcd.js";
-import * as _207 from "./marketmap/v1/query.rpc.Query.js";
-import * as _208 from "./oracle/v1/query.rpc.Query.js";
-import * as _209 from "./marketmap/v1/tx.rpc.msg.js";
-import * as _210 from "./oracle/v1/tx.rpc.msg.js";
-import * as _232 from "./lcd.js";
-import * as _233 from "./rpc.query.js";
-import * as _234 from "./rpc.tx.js";
+import * as _87 from "./marketmap/module/v1/module.js";
+import * as _88 from "./marketmap/v1/genesis.js";
+import * as _89 from "./marketmap/v1/market.js";
+import * as _90 from "./marketmap/v1/params.js";
+import * as _91 from "./marketmap/v1/query.js";
+import * as _92 from "./marketmap/v1/tx.js";
+import * as _93 from "./oracle/module/v1/module.js";
+import * as _94 from "./oracle/v1/genesis.js";
+import * as _95 from "./oracle/v1/query.js";
+import * as _96 from "./oracle/v1/tx.js";
+import * as _97 from "./types/v1/currency_pair.js";
+import * as _202 from "./marketmap/v1/tx.amino.js";
+import * as _203 from "./oracle/v1/tx.amino.js";
+import * as _204 from "./marketmap/v1/tx.registry.js";
+import * as _205 from "./oracle/v1/tx.registry.js";
+import * as _206 from "./marketmap/v1/query.lcd.js";
+import * as _207 from "./oracle/v1/query.lcd.js";
+import * as _208 from "./marketmap/v1/query.rpc.Query.js";
+import * as _209 from "./oracle/v1/query.rpc.Query.js";
+import * as _210 from "./marketmap/v1/tx.rpc.msg.js";
+import * as _211 from "./oracle/v1/tx.rpc.msg.js";
+import * as _233 from "./lcd.js";
+import * as _234 from "./rpc.query.js";
+import * as _235 from "./rpc.tx.js";
 export namespace slinky {
   export namespace marketmap {
     export namespace module {
       export const v1 = {
-        ..._86
+        ..._87
       };
     }
     export const v1 = {
-      ..._87,
       ..._88,
       ..._89,
       ..._90,
       ..._91,
-      ..._201,
-      ..._203,
-      ..._205,
-      ..._207,
-      ..._209
-    };
-  }
-  export namespace oracle {
-    export namespace module {
-      export const v1 = {
-        ..._92
-      };
-    }
-    export const v1 = {
-      ..._93,
-      ..._94,
-      ..._95,
+      ..._92,
       ..._202,
       ..._204,
       ..._206,
@@ -60,14 +43,31 @@ export namespace slinky {
       ..._210
     };
   }
+  export namespace oracle {
+    export namespace module {
+      export const v1 = {
+        ..._93
+      };
+    }
+    export const v1 = {
+      ..._94,
+      ..._95,
+      ..._96,
+      ..._203,
+      ..._205,
+      ..._207,
+      ..._209,
+      ..._211
+    };
+  }
   export namespace types {
     export const v1 = {
-      ..._96
+      ..._97
     };
   }
   export const ClientFactory = {
-    ..._232,
     ..._233,
-    ..._234
+    ..._234,
+    ..._235
   };
 }

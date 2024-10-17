740574395b703298895938ffda310dccf2501310
Maks Nabokov <max.nab@equilibrium.io>
2024-10-17 10:00:36 +0300

    warden precompile tests

---
+++ tests/cases/warden_precompile.go
@@ -0,0 +1,301 @@
+package cases
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/hex"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"github.com/warden-protocol/wardenprotocol/precompiles/warden"
+
+	"github.com/warden-protocol/wardenprotocol/tests/framework"
+	"github.com/warden-protocol/wardenprotocol/tests/framework/checks"
+	"github.com/warden-protocol/wardenprotocol/tests/framework/exec"
+)
+
+func init() {
+	Register(&Test_WardenPrecompile{})
+}
+
+type Test_WardenPrecompile struct {
+	w *exec.WardenNode
+}
+
+func (c *Test_WardenPrecompile) Setup(t *testing.T, ctx context.Context, build framework.BuildResult) {
+	c.w = exec.NewWardenNode(t, build.Wardend)
+
+	go c.w.Start(t, ctx, "./testdata/snapshot-many-users")
+	c.w.WaitRunnning(t)
+
+	// c.w.PrintLogsAtTheEnd(t, ctx)
+}
+
+func (c *Test_WardenPrecompile) Run(t *testing.T, ctx context.Context, build framework.BuildResult) {
+	alice := exec.NewWardend(c.w, "alice")
+	bob := exec.NewWardend(c.w, "bob")
+	// dave := exec.NewWardend(c.w, "dave")
+
+	// client := TestGRPCClient(*c.w.GRPCClient(t))
+	evmClient := c.w.EthClient(t)
+	iWardenClient, err := warden.NewIWarden(common.HexToAddress(warden.PrecompileAddress), evmClient)
+	require.NoError(t, err)
+
+	t.Run("work with keychains", func(t *testing.T) {
+		// new keychain
+		keyReq := []warden.Coin{}
+		sigReq := []warden.Coin{}
+		keychainFees := warden.KeychainFees{
+			KeyReq: keyReq,
+			SigReq: sigReq,
+		}
+		keychainName := "testKeychain"
+		keychainDescription := "description"
+		keychainUrl := "test.url"
+		keychainKeybaseId := "1234567890ABCDEF"
+		_, err := iWardenClient.NewKeychain(alice.TransactOps(t, ctx, evmClient), keychainName, keychainFees, keychainDescription, keychainUrl, keychainKeybaseId)
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keychain, err := iWardenClient.KeychainById(alice.CallOps(t), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		keychainAdmins := []string{}
+		keychainWriters := []string{}
+		keychainAdmins = append(keychainAdmins, alice.Address(t))
+		require.Equal(t, warden.Keychain{
+			Id:          1,
+			Creator:     alice.Address(t),
+			Name:        keychainName,
+			Admins:      keychainAdmins,
+			Writers:     keychainWriters,
+			Fees:        keychainFees,
+			Description: keychainDescription,
+			Url:         keychainUrl,
+			KeybaseId:   keychainKeybaseId,
+		}, keychain)
+		// add keychain writer
+		_, err = iWardenClient.AddKeychainWriter(alice.TransactOps(t, ctx, evmClient), 1, alice.EthAddress(t))
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keychain, err = iWardenClient.KeychainById(alice.CallOps(t), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		keychainWriters = []string{alice.Address(t)}
+		require.Equal(t, warden.Keychain{
+			Id:          1,
+			Creator:     alice.Address(t),
+			Name:        keychainName,
+			Admins:      keychainAdmins,
+			Writers:     keychainWriters,
+			Fees:        keychainFees,
+			Description: keychainDescription,
+			Url:         keychainUrl,
+			KeybaseId:   keychainKeybaseId,
+		}, keychain)
+		// add keychain admin
+		_, err = iWardenClient.AddKeychainAdmin(alice.TransactOps(t, ctx, evmClient), 1, bob.EthAddress(t))
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keychain, err = iWardenClient.KeychainById(alice.CallOps(t), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		keychainAdmins = []string{alice.Address(t), bob.Address(t)}
+		require.Equal(t, warden.Keychain{
+			Id:          1,
+			Creator:     alice.Address(t),
+			Name:        keychainName,
+			Admins:      keychainAdmins,
+			Writers:     keychainWriters,
+			Fees:        keychainFees,
+			Description: keychainDescription,
+			Url:         keychainUrl,
+			KeybaseId:   keychainKeybaseId,
+		}, keychain)
+		// remove keychain admin
+		_, err = iWardenClient.RemoveKeychainAdmin(alice.TransactOps(t, ctx, evmClient), 1, alice.EthAddress(t))
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keychain, err = iWardenClient.KeychainById(alice.CallOps(t), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		keychainAdmins = []string{bob.Address(t)}
+		require.Equal(t, warden.Keychain{
+			Id:          1,
+			Creator:     alice.Address(t),
+			Name:        keychainName,
+			Admins:      keychainAdmins,
+			Writers:     keychainWriters,
+			Fees:        keychainFees,
+			Description: keychainDescription,
+			Url:         keychainUrl,
+			KeybaseId:   keychainKeybaseId,
+		}, keychain)
+		// update keychain
+		keychainName = "newRestKeychain"
+		keychainDescription = "newDescription"
+		keychainUrl = "new.test.url"
+		keychainKeybaseId = "ABCDEF1234567890"
+		keyReq = []warden.Coin{{
+			Denom:  "award",
+			Amount: new(big.Int).SetInt64(1),
+		}}
+		sigReq = []warden.Coin{{
+			Denom:  "award",
+			Amount: new(big.Int).SetInt64(1),
+		}}
+		keychainFees = warden.KeychainFees{
+			KeyReq: keyReq,
+			SigReq: sigReq,
+		}
+		_, err = iWardenClient.UpdateKeychain(bob.TransactOps(t, ctx, evmClient), 1, keychainName, keychainFees, keychainDescription, keychainUrl, keychainKeybaseId)
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keychain, err = iWardenClient.KeychainById(alice.CallOps(t), 1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		keychainAdmins = []string{bob.Address(t)}
+		require.Equal(t, warden.Keychain{
+			Id:          1,
+			Creator:     alice.Address(t),
+			Name:        keychainName,
+			Admins:      keychainAdmins,
+			Writers:     keychainWriters,
+			Fees:        keychainFees,
+			Description: keychainDescription,
+			Url:         keychainUrl,
+			KeybaseId:   keychainKeybaseId,
+		}, keychain)
+	})
+
+	t.Run("work with space", func(t *testing.T) {
+		additionalOwners := []common.Address{}
+		_, err := iWardenClient.NewSpace(alice.TransactOps(t, ctx, evmClient), 0, 0, 0, 0, additionalOwners)
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		space, err := iWardenClient.SpaceById(alice.CallOps(t), 2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		spaceOwners := []string{alice.Address(t)}
+		require.Equal(t, warden.Space{
+			Id:                     2,
+			Creator:                alice.Address(t),
+			Owners:                 spaceOwners,
+			Nonce:                  0,
+			ApproveAdminTemplateId: 0,
+			RejectAdminTemplateId:  0,
+			ApproveSignTemplateId:  0,
+			RejectSignTemplateId:   0,
+		}, space)
+		// add space owner //
+		// remove space owner //
+		// update space //
+	})
+
+	t.Run("work with keys", func(t *testing.T) {
+		// new key request //
+		newKeyReqTx := alice.Tx(t, "warden new-action new-key-request --space-id 1 --keychain-id 1 --key-type 1 --max-keychain-fees \"1award\" --nonce 0")
+		checks.SuccessTx(t, newKeyReqTx)
+		// fulfil key request
+		pubKey, err := hex.DecodeString("a5d4105f9c22b101f3bb0f53f4b96e9b931dbae0542858a98b8ff9aeedc1d09f09")
+		if err != nil {
+			panic(err)
+		}
+		_, err = iWardenClient.FulfilKeyRequest(alice.TransactOps(t, ctx, evmClient), 1, pubKey)
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keyRequest, err := iWardenClient.KeyRequestById(alice.CallOps(t), 1)
+		require.NoError(t, err)
+		deductedKeychainFees := []warden.Coin{{
+			Denom:  "award",
+			Amount: new(big.Int).SetInt64(1),
+		}}
+		require.Equal(t, warden.KeyRequest{
+			Id:                   1,
+			Creator:              alice.Address(t),
+			SpaceId:              1,
+			KeychainId:           1,
+			KeyType:              1,
+			Status:               2,
+			ApproveTemplateId:    0,
+			RejectTemplateId:     0,
+			DeductedKeychainFees: deductedKeychainFees,
+		}, keyRequest)
+		// reject key request
+		newKeyReqTx = alice.Tx(t, "warden new-action new-key-request --space-id 1 --keychain-id 1 --key-type 1 --max-keychain-fees \"1award\" --nonce 0")
+		checks.SuccessTx(t, newKeyReqTx)
+		_, err = iWardenClient.RejectKeyRequest(alice.TransactOps(t, ctx, evmClient), 2, "test reject reason")
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		keyRequest, err = iWardenClient.KeyRequestById(alice.CallOps(t), 2)
+		require.NoError(t, err)
+		require.Equal(t, warden.KeyRequest{
+			Id:                   2,
+			Creator:              alice.Address(t),
+			SpaceId:              1,
+			KeychainId:           1,
+			KeyType:              1,
+			Status:               3,
+			ApproveTemplateId:    0,
+			RejectTemplateId:     0,
+			RejectReason:         "test reject reason",
+			DeductedKeychainFees: deductedKeychainFees,
+		}, keyRequest)
+		// new sign request //
+		// fulfil sign request
+		newSignReqTx := alice.Tx(t, "warden new-action new-sign-request --key-id 1 --input 'HoZ4Z+ZU7Zd08kUR5NcbtFZrmGKF18mSBJ29dg0qI44=' --max-keychain-fees \"1award\" --nonce 0")
+		checks.SuccessTx(t, newSignReqTx)
+		signedData, err := base64.StdEncoding.DecodeString("LKu131U23Q5Ke7jJscb57zdSmuZD27a4VeZ+/hwf7ShOLo4ozUc36pvNT14+a1s09k1PbPihrFbK29J00Jh3tgA=")
+		if err != nil {
+			panic(err)
+		}
+		_, err = iWardenClient.FulfilSignRequest(alice.TransactOps(t, ctx, evmClient), 1, signedData)
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		signRequest, err := iWardenClient.SignRequestById(alice.CallOps(t), 1)
+		require.NoError(t, err)
+		dataForSigning, err := base64.StdEncoding.DecodeString("HoZ4Z+ZU7Zd08kUR5NcbtFZrmGKF18mSBJ29dg0qI44=")
+		if err != nil {
+			panic(err)
+		}
+		encryptionKey := []byte{}
+		require.Equal(t, warden.SignRequest{
+			Id:                   1,
+			Creator:              alice.Address(t),
+			KeyId:                1,
+			DataForSigning:       dataForSigning,
+			Status:               2,
+			Result:               signedData,
+			EncryptionKey:        encryptionKey,
+			DeductedKeychainFees: deductedKeychainFees,
+		}, signRequest)
+		// update key //
+		// reject sign request
+		newSignReqTx = alice.Tx(t, "warden new-action new-sign-request --key-id 1 --input 'HoZ4Z+ZU7Zd08kUR5NcbtFZrmGKF18mSBJ29dg0qI44=' --max-keychain-fees \"1award\" --nonce 0")
+		checks.SuccessTx(t, newSignReqTx)
+		_, err = iWardenClient.RejectSignRequest(alice.TransactOps(t, ctx, evmClient), 2, "test reject reason")
+		require.NoError(t, err)
+		time.Sleep(4 * time.Second)
+		signRequest, err = iWardenClient.SignRequestById(alice.CallOps(t), 2)
+		require.NoError(t, err)
+		result := []byte{}
+		require.Equal(t, warden.SignRequest{
+			Id:                   2,
+			Creator:              alice.Address(t),
+			KeyId:                1,
+			DataForSigning:       dataForSigning,
+			Status:               3,
+			Result:               result,
+			EncryptionKey:        encryptionKey,
+			DeductedKeychainFees: deductedKeychainFees,
+		}, signRequest)
+	})
+}

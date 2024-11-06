908b96968ea2c77a28a97ee6535201935eb5a931
Archie <artur.abliazimov@gmail.com>
2024-11-06 15:46:48 +0300

    Automated Orders: add a layer to work with blockchains (#1039)
    * Automated Orders: add a layer to work with blockchains
    
    * Fix namings
    
    ---------
    
    Co-authored-by: Artur Abliazimov <artur.abliazimov@equilibrium.io>

--- automated-orders/packages/relayer/src/index.ts
+++ automated-orders/packages/relayer/src/index.ts
@@ -1,9 +1,29 @@
-import { logError, logInfo } from '@warden-automated-orders/utils';
+import { EvmClient, NewSignatureProcessor, WardenClient } from '@warden-automated-orders/blockchain';
+import { logError } from '@warden-automated-orders/utils';
+
+import { config } from './config/config.js';
 
 async function main() {
-  logInfo('main');
+  const warden = new WardenClient({
+    rpcURL: config.WARDEN_RPC_URL,
+    pollingIntervalMsec: config.WARDEN_POLLING_INTERVAL_MSEC,
+  });
+
+  const ethereum = new EvmClient({
+    rpcURL: config.ETHEREUM_NODE_RPC,
+  });
+
+  const newSignatureRequestProcess = new NewSignatureProcessor(
+    ethereum,
+    warden.pollSignatureRequests.bind(warden),
+  ).start();
+
+  await Promise.all([newSignatureRequestProcess]);
 }
 
 main()
-  .catch((e) => logError(e))
+  .catch((e) => {
+    logError(e);
+    process.exit(1);
+  })
   .finally(() => process.exit());

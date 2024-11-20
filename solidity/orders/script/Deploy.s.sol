e74b59380090d21856bba81a45db52e65d1b4bc5
Maks Nabokov <max.nab@equilibrium.io>
2024-11-20 15:58:12 +0300

    script to deploy registry and factory

---
+++ solidity/orders/script/Deploy.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.25 <0.9.0;
+
+import { Script } from "forge-std/src/Script.sol";
+import { Registry } from "../src/Registry.sol";
+import { OrderFactory } from "../src/OrderFactory.sol";
+
+contract Deploy is Script {
+    address internal broadcaster;
+    address internal scheduler;
+    address internal factoryOwner;
+    address internal registryAddress;
+
+    constructor() {
+        (broadcaster,) = deriveRememberKey({ mnemonic: vm.envString("MNEMONIC"), index: 0 });
+        scheduler = vm.envAddress("SCHEDULER_ADDRESS");
+        factoryOwner = vm.envAddress("FACTORY_OWNER_ADDRESS");
+        registryAddress = vm.envOr("REGISTRY_ADDRESS", address(0));
+    }
+
+    function run() external {
+        vm.startBroadcast(broadcaster);
+
+        Registry registry;
+        if (registryAddress == address(0)) {
+            registry = new Registry();
+        } else {
+            registry = Registry(registryAddress);
+        }
+        new OrderFactory(address(registry), scheduler, factoryOwner);
+
+        vm.stopBroadcast();
+    }
+}

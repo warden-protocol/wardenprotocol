27996b889bd35e1ed849fa3092263d76202e923c
Antonio Pitasi <antonio@pitasi.dev>
2024-11-25 17:33:24 +0100

    feat(x/async): implement AddFuture msg handler
    This adds the handler for MsgAddFuture, and a FutureKeeper struct for
    abstracting the underlying storage and exposing more convenient API for
    retrieving the result of a Future and all the Futures created by a
    specific address.

---
+++ warden/x/async/keeper/msg_server_add_future.go
@@ -0,0 +1,41 @@
+package keeper
+
+import (
+	"context"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
+)
+
+func (k msgServer) AddFuture(ctx context.Context, msg *types.MsgAddFuture) (*types.MsgAddFutureResponse, error) {
+	if msg.Handler == "" {
+		return nil, errorsmod.Wrapf(types.ErrInvalidHandler, "cannot be empty")
+	}
+
+	if len(msg.Input) == 0 {
+		return nil, errorsmod.Wrapf(types.ErrInvalidFutureInput, "cannot be empty")
+	}
+
+	id, err := k.futures.Append(ctx, &types.Future{
+		Creator: msg.Creator,
+		Handler: msg.Handler,
+		Input:   msg.Input,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	if err := sdkCtx.EventManager().EmitTypedEvent(&types.EventCreateFuture{
+		Id:      id,
+		Creator: msg.Creator,
+		Handler: msg.Handler,
+	}); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgAddFutureResponse{
+		Id: id,
+	}, nil
+}

d3c4d5939c1ea86fcb0f42578e03306feaea0fbe
Antonio Pitasi <antonio@pitasi.dev>
2024-11-25 17:36:28 +0100

    future(x/async): implement queries
    This implements the remaining queries for the module:
    - Futures returns all the futures;
    - FutureById returns one future by the specified id;
    - PendingFutures returns all the futures that do not have a result.
    
    It uses the FutureKeeper introduced in the previous commit.

---
+++ warden/x/async/keeper/query_pending_futures.go
@@ -0,0 +1,36 @@
+package keeper
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/cosmos/cosmos-sdk/types/query"
+	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
+)
+
+func (k Keeper) PendingFutures(ctx context.Context, req *types.QueryPendingFuturesRequest) (*types.QueryPendingFuturesResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	futures, pageRes, err := query.CollectionFilteredPaginate(ctx, k.futures.Futures(), req.Pagination, func(key uint64, value types.Future) (bool, error) {
+		hasResult, err := k.futures.HasResult(ctx, value.Id)
+		if err != nil {
+			return false, err
+		}
+		return !hasResult, nil
+	}, func(key uint64, value types.Future) (types.Future, error) {
+		return value, nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryPendingFuturesResponse{
+		Pagination: pageRes,
+		Futures:    futures,
+	}, nil
+}

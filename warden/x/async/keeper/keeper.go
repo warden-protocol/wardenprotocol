38bf93966f0bc65122a5c8f79536c6d4bc3b98e3
Antonio Pitasi <antonio@pitasi.dev>
2024-11-25 17:29:52 +0100

    feat(x/async): scaffold new module code
    No features are added to x/async yet. See following commits.

---
+++ warden/x/async/keeper/keeper.go
@@ -0,0 +1,63 @@
+package keeper
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"cosmossdk.io/collections"
+	"cosmossdk.io/core/store"
+	"cosmossdk.io/log"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
+)
+
+type (
+	Keeper struct {
+		cdc          codec.Codec
+		storeService store.KVStoreService
+		logger       log.Logger
+
+		// the address capable of executing a MsgUpdateParams message. Typically, this
+		// should be the x/gov module account.
+		authority string
+	}
+)
+
+func NewKeeper(
+	cdc codec.Codec,
+	storeService store.KVStoreService,
+	logger log.Logger,
+	authority string,
+) Keeper {
+	if _, err := sdk.AccAddressFromBech32(authority); err != nil {
+		panic(fmt.Sprintf("invalid authority address: %s", authority))
+	}
+
+	sb := collections.NewSchemaBuilder(storeService)
+
+	_, err := sb.Build()
+	if err != nil {
+		panic(fmt.Sprintf("failed to build schema: %s", err))
+	}
+
+	return Keeper{
+		cdc:          cdc,
+		storeService: storeService,
+		authority:    authority,
+		logger:       logger,
+	}
+}
+
+// GetAuthority returns the module's authority.
+func (k Keeper) GetAuthority() string {
+	return k.authority
+}
+
+// Logger returns a module-specific logger.
+func (k Keeper) Logger() log.Logger {
+	return k.logger.With("module", fmt.Sprintf("x/%s", types.ModuleName))
+}
+
---
+++ warden/x/async/module/genesis_test.go
@@ -0,0 +1,29 @@
+package async_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "github.com/warden-protocol/wardenprotocol/warden/testutil/keeper"
+	"github.com/warden-protocol/wardenprotocol/warden/testutil/nullify"
+	async "github.com/warden-protocol/wardenprotocol/warden/x/async/module"
+	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.AsyncKeeper(t)
+	async.InitGenesis(ctx, k, genesisState)
+	got := async.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}

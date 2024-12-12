f58cbe356dcdfc2e94d76a8f64f72840480751dd
backsapc <backsapce@hotmail.com>
2024-12-12 14:03:17 +0300

    Made client to poll all requests everytime, moved cache to processor (#1143)

--- automated-orders/packages/blockchain-library/src/processors/newSignatureProcessor.ts
+++ automated-orders/packages/blockchain-library/src/processors/newSignatureProcessor.ts
@@ -5,37 +5,51 @@ import { INewSignatureRequest } from '../types/warden/newSignatureRequest.js';
 import { Processor } from './processor.js';
 import { ExponentialBackoff, handleAll, IPolicy, retry } from 'cockatiel';
 import { WardenRegistryClient } from '../clients/registry.js';
+import { LRUCache } from 'lru-cache';
 
 export class NewSignatureProcessor extends Processor<INewSignatureRequest> {
   private retryPolicy: IPolicy;
+  private recentSignRequests: LRUCache<bigint, bigint>;
 
   constructor(
     private evm: EvmClient,
     private registryClient: WardenRegistryClient,
     generator: () => AsyncGenerator<INewSignatureRequest, unknown, unknown>,
+    seenRequestsCacheSize: number
   ) {
     super(generator);
 
     const retryPolicy = retry(handleAll, { maxAttempts: 3, backoff: new ExponentialBackoff() });
     // TODO AT: Add circuit breaker
     this.retryPolicy = retryPolicy;
+
+    this.recentSignRequests = new LRUCache<bigint, bigint>({
+      max: seenRequestsCacheSize,
+    });
   }
 
   async handle(data: INewSignatureRequest): Promise<void> {
+    if(this.recentSignRequests.has(data.id)) { 
+      logInfo(`Skip already seen transaction ${data.transactionHash}`);
+      return;
+    }
+    
+    logInfo(`New Signature request ${serialize(data)}`);
+
     try {
-      logInfo(`New Signature request ${serialize(data)}`);
-      
-      await this.retryPolicy.execute(async () => { 
+      await this.retryPolicy.execute(async () => {
         const transactionExists = await this.evm.transactionExists(data.transactionHash);
-        if(transactionExists) {
+        if (transactionExists) {
           logInfo(`Transaction ${data.transactionHash} already exists`);
+          this.recentSignRequests.set(data.id, data.id);
           return;
         }
 
         const transaction = await this.registryClient.getTransaction(data.transactionHash);
-        if(transaction) {
+        if (transaction) {
           await this.evm.broadcastTx(transaction, data.signature);
-        } else { 
+          this.recentSignRequests.set(data.id, data.id);
+        } else {
           logError(`Transaction with hash "${data.transactionHash}" not found in registry`)
         }
       });
--- automated-orders/packages/blockchain-library/src/types/warden/newSignatureRequest.ts
+++ automated-orders/packages/blockchain-library/src/types/warden/newSignatureRequest.ts
@@ -1,6 +1,7 @@
 import { Hex } from "viem";
 
 export interface INewSignatureRequest {
+  id: bigint;
   signature: Hex;
   transactionHash: Hex;
 }

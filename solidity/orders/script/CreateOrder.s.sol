4eed97fbe42823a42cecafefdd2cc591e506d878
Andrei Beliaev <andrei.beliaev@eqlab.online>
2024-12-17 20:18:03 +0500

    Add salt arg to CreateOrder script

--- solidity/orders/script/CreateOrder.s.sol
+++ solidity/orders/script/CreateOrder.s.sol
@@ -34,7 +34,8 @@ contract CreateOrder is Script {
         uint64 spaceNonce,
         uint64 actionTimeoutHeight,
         bytes calldata expectedApproveExpression,
-        bytes calldata expectedRejectExpression
+        bytes calldata expectedRejectExpression,
+        bytes32 salt
     )
         external
     {
@@ -65,7 +66,6 @@ contract CreateOrder is Script {
         mockSlinkyPrecompile.setPrice(pricePair.base, pricePair.quote, thresholdPrice);
         vm.etch(IWARDEN_PRECOMPILE_ADDRESS, address(wPrecompile).code);
         vm.startBroadcast(broadcaster);
-        bytes32 salt = keccak256(abi.encodePacked(broadcaster, block.number));
         FACTORY.createOrder(orderData, maxKeychainFees, OrderType.Basic, salt);
 
         vm.stopBroadcast();

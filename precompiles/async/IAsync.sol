97c74f81d72288d4278de7e3aaef84448751f548
Aleksandr Tretiakov <backsapce@hotmail.com>
2024-12-24 16:07:57 +0300

    Added async precompile

---
+++ precompiles/async/IAsync.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.18;
+
+import "../common/Types.sol";
+
+/// @dev The IAsync contract's address.
+address constant IASYNC_PRECOMPILE_ADDRESS = 0x0000000000000000000000000000000000000903;
+
+/// @dev The IAsync contract's instance.
+IAsync constant IASYNC_CONTRACT = IAsync(IASYNC_PRECOMPILE_ADDRESS);
+
+struct Future {
+    uint64 id;
+    address creator;
+    string handler;
+    bytes input;
+}
+
+enum FutureVoteType {
+    Unspecified,
+    Verified,
+    Rejected
+}
+
+struct FutureVote {
+    uint64 futureId;
+    bytes Voter;
+    FutureVoteType vote;
+}
+
+struct FutureResult { 
+    uint64 id;
+    bytes output;
+    bytes submitter;
+}
+
+struct FutureResponse {
+    Future future;
+    FutureVote[] votes;
+    FutureResult result;
+}
+
+struct PendingFuturesResponse {
+    Types.PageResponse pagination;
+    Future[] futures;
+}
+
+struct FuturesResponse {
+    Types.PageResponse pagination;
+    FutureResponse[] futures;
+}
+
+struct FutureByIdResponse {
+    Future future;
+}
+
+/**
+ * @author Warden Team
+ * @title x/async Interface
+ * @dev The interface through which users and solidity contracts will interact with x/async.
+ * @custom:address 0x0000000000000000000000000000000000000903
+ */
+interface IAsync {
+    /// @dev Defines a method to add a future.
+    /// @param handler The unique name of the handler
+    /// @param input The handler's input
+    /// @return futureId The id of the future
+    function addFuture(
+        string calldata handler,
+        bytes calldata input
+    ) external returns (uint64 futureId);
+
+    /// @dev Defines a method to query future by id.
+    /// @param futureId The pagination details
+    /// @return response The future
+    function futureById(
+        uint64 futureId
+    ) external view returns (FutureByIdResponse memory response);
+
+    /// @dev Defines a method to query futures.
+    /// @param pagination The pagination details
+    /// @param creator Optional creator address filter
+    /// @return response The paged futures
+    function futures(
+        Types.PageRequest calldata pagination,
+        address creator
+    ) external view returns (FuturesResponse memory response);
+
+    /// @dev Defines a method to query pending futures.
+    /// @param pagination The pagination details
+    /// @return response The paged futures
+    function pendingFutures(
+        Types.PageRequest calldata pagination
+    ) external view returns (PendingFuturesResponse memory response);
+
+    /// @dev CreateFuture defines an Event emitted when a future is created.
+    /// @param creator The address of the creator
+    /// @param futureId The future Id
+    /// @param handler The name of the handler
+    event CreateFuture(
+        uint64 indexed futureId,
+        address indexed creator,
+        string handler
+    );
+}
---
+++ precompiles/async/types.go
@@ -0,0 +1,152 @@
+package async
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/ethereum/go-ethereum/common"
+
+	precommon "github.com/warden-protocol/wardenprotocol/precompiles/common"
+	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
+)
+
+// ActionsInput needed to unmarshal Pagination field and pass it to types.QueryActionsRequest
+type FuturesInput struct {
+	Pagination query.PageRequest `abi:"pagination"`
+	Creator    common.Address    `abi:"creator"`
+}
+
+// FromResponse needed to map QueryActionsResponse to ActionsResponse
+func (r *FuturesResponse) FromResponse(res *types.QueryFuturesResponse) (FuturesResponse, error) {
+	if res != nil {
+		futures := make([]FutureResponse, 0, len(res.Futures))
+		for _, action := range res.Futures {
+			mappedFuture, err := mapFutureResponse(action)
+			if err != nil {
+				return FuturesResponse{}, err
+			}
+
+			futures = append(futures, mappedFuture)
+		}
+
+		r.Futures = futures
+		r.Pagination = mapPageResponse(res.Pagination)
+	}
+
+	return *r, nil
+}
+
+// FromResponse needed to map QueryActionByIdResponse to ActionByIdResponse
+func (r *PendingFuturesResponse) FromResponse(res *types.QueryPendingFuturesResponse) (PendingFuturesResponse, error) {
+	if res != nil {
+		futures := make([]Future, 0, len(res.Futures))
+		for _, action := range res.Futures {
+			mappedFuture, err := mapFuture(action)
+			if err != nil {
+				return PendingFuturesResponse{}, err
+			}
+
+			futures = append(futures, mappedFuture)
+		}
+
+		r.Futures = futures
+		r.Pagination = mapPageResponse(res.Pagination)
+	}
+
+	return *r, nil
+}
+
+// FromResponse needed to map QueryActionByIdResponse to ActionByIdResponse
+func (r *FutureByIdResponse) FromResponse(res *types.QueryFutureByIdResponse) (FutureByIdResponse, error) {
+	if res != nil {
+		mappedFuture, err := mapFuture(res.Future)
+		if err != nil {
+			return FutureByIdResponse{}, err
+		}
+
+		r.Future = mappedFuture
+	}
+
+	return *r, nil
+}
+
+func mapFuture(action types.Future) (Future, error) {
+	creator, err := precommon.AddressFromBech32Str(action.Creator)
+	if err != nil {
+		return Future{}, fmt.Errorf("invalid creator: %w", err)
+	}
+
+	return Future{
+		Id:      action.Id,
+		Creator: creator,
+		Handler: action.Handler,
+		Input:   action.Input,
+	}, nil
+}
+
+func mapFutureResponse(futureResponse types.FutureResponse) (FutureResponse, error) {
+	future, err := mapFuture(futureResponse.Future)
+	if err != nil {
+		return FutureResponse{}, err
+	}
+
+	votes, err := mapVotes(futureResponse.Votes)
+	if err != nil {
+		return FutureResponse{}, err
+	}
+
+	futureResult, err := mapFutureResult(futureResponse.Result)
+	if err != nil {
+		return FutureResponse{}, err
+	}
+
+	return FutureResponse{
+		Future: future,
+		Votes:  votes,
+		Result: futureResult,
+	}, nil
+}
+
+func mapVotes(values []types.FutureVote) ([]FutureVote, error) {
+	result := make([]FutureVote, 0, len(values))
+	for _, v := range values {
+		mappedTemplate, err := mapVote(v)
+		if err != nil {
+			return nil, err
+		}
+
+		result = append(result, mappedTemplate)
+	}
+	return result, nil
+}
+
+func mapVote(value types.FutureVote) (FutureVote, error) {
+	return FutureVote{
+		Voter:    value.Voter,
+		FutureId: value.FutureId,
+		Vote:     uint8(value.Vote),
+	}, nil
+}
+
+func mapFutureResult(value *types.FutureResult) (FutureResult, error) {
+	if value == nil {
+		return FutureResult{}, nil
+	}
+
+	return FutureResult{
+		Id:        value.Id,
+		Output:    value.Output,
+		Submitter: value.Submitter,
+	}, nil
+}
+
+func mapPageResponse(value *query.PageResponse) TypesPageResponse {
+	if value == nil {
+		return TypesPageResponse{}
+	}
+
+	return TypesPageResponse{
+		NextKey: value.NextKey,
+		Total:   value.Total,
+	}
+}

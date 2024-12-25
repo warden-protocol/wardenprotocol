99c2808aec16879bfeb3a70c152e905affaa6f92
backsapc <backsapce@hotmail.com>
2024-12-25 12:11:44 +0300

    Added async precompile (#1160)
    * Added async precompile
    
    * Fixed comments

--- warden/app/legacy.go
+++ warden/app/legacy.go
@@ -340,7 +340,12 @@ func (app *App) registerLegacyModules(appOpts servertypes.AppOptions, wasmOpts [
 	// NOTE: we are just adding the default Ethereum precompiles here.
 	// Additional precompiles could be added if desired.
 	precompiles := maps.Clone(vm.PrecompiledContractsBerlin)
-	wardenprecompiles, err := wardenprecompiles.NewWardenPrecompiles(app.WardenKeeper, app.ActKeeper, *app.OracleKeeper)
+	wardenprecompiles, err := wardenprecompiles.NewWardenPrecompiles(
+		app.WardenKeeper,
+		app.ActKeeper,
+		*app.OracleKeeper,
+		app.AsyncKeeper,
+	)
 	if err != nil {
 		panic(err)
 	}

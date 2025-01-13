d75c900c780d948baba46cb6f0a5009f5e7e8817
Antonio Pitasi <antonio@pitasi.dev>
2025-01-13 09:42:16 +0100

    Reapply "feat(prophet): initial version" (#1158)
    This reverts commit 7a1f7bd528c86afaab2ed9a7616c9ebdc9cf06bd.

---
+++ prophet/registry.go
@@ -0,0 +1,29 @@
+package prophet
+
+import "sync"
+
+// registry is a global registry of futures handlers.
+var registry = r{
+	futures: make(map[string]FutureHandler),
+}
+
+type r struct {
+	rw      sync.RWMutex
+	futures map[string]FutureHandler
+}
+
+// Register registers a new future handler with the given unique name.
+func Register(name string, future FutureHandler) {
+	registry.rw.Lock()
+	defer registry.rw.Unlock()
+	if _, found := registry.futures[name]; found {
+		panic("future already registered")
+	}
+	registry.futures[name] = future
+}
+
+func getHandler(name string) FutureHandler {
+	registry.rw.RLock()
+	defer registry.rw.RUnlock()
+	return registry.futures[name]
+}

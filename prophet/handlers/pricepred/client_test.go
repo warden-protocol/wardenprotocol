1bcb635df046a673ce8420e85a3c5eed25b9d78d
Antonio Pitasi <antonio@pitasi.dev>
2025-01-29 12:05:35 +0100

    feat(x/async/pricepred): add backtesting metrics to client

--- prophet/handlers/pricepred/client_test.go
+++ prophet/handlers/pricepred/client_test.go
@@ -33,3 +33,25 @@ func TestPredict(t *testing.T) {
 		require.NotZero(t, pred, "prediction for %t is zero", token)
 	}
 }
+
+func TestBacktesting(t *testing.T) {
+	t.Skip("this test relies on external HTTP call")
+
+	c := newClient(http.DefaultClient, &url.URL{
+		Scheme: "https",
+		Host:   "tpc.devnet.wardenprotocol.org",
+	})
+
+	ctx := context.Background()
+	res, err := c.Backtesting(ctx, BacktestingRequest{
+		SolverInput: RequestSolverInput{
+			Tokens:        []string{"bitcoin", "tether", "uniswap"},
+			TargetDate:    "2025-01-28",
+			AdversaryMode: false,
+		},
+		FalsePositiveRate: 0.01,
+	})
+	require.NoError(t, err)
+
+	require.NotZero(t, res.SolverOutput.Metrics)
+}

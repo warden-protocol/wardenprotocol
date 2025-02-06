5fee17b1c5e9db17867d0df7006b3e062653dfea
Maks Nabokov <max.nab@equilibrium.io>
2025-02-06 11:06:42 +0300

    add handlers into async genesis

--- warden/x/async/keeper/genesis.go
+++ warden/x/async/keeper/genesis.go
@@ -6,9 +6,14 @@ import (
 )
 
 func (k *Keeper) ImportState(ctx sdk.Context, genState types.GenesisState) error {
-	return nil
+	return k.handlers.Import(ctx, genState.Handlers)
 }
 
 func (k *Keeper) ExportState(ctx sdk.Context, genState *types.GenesisState) error {
+	handlers, err := k.handlers.Export(ctx)
+	if err != nil {
+		return err
+	}
+	genState.Handlers = handlers
 	return nil
 }

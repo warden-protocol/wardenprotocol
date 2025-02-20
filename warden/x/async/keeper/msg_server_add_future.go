f6085927b83a32582d0f67e9115a31cacd5d8f5b
Maks Nabokov <max.nab@equilibrium.io>
2025-02-20 18:44:15 +0300

    check if callback address is contract

--- warden/x/async/keeper/msg_server_add_future.go
+++ warden/x/async/keeper/msg_server_add_future.go
@@ -5,6 +5,7 @@ import (
 
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	precommon "github.com/warden-protocol/wardenprotocol/precompiles/common"
 	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
 )
 
@@ -25,6 +26,22 @@ func (k msgServer) AddFuture(ctx context.Context, msg *types.MsgAddFuture) (*typ
 	})
 	if err != nil {
 		return nil, err
+	}
+
+	if msg.Callback != "" {
+		address, err := precommon.AddressFromBech32Str(msg.Callback)
+
+		if err != nil {
+			return nil, errorsmod.Wrapf(types.ErrInvalidCallback, "invalid callback address: %s", err)
+		}
+
+		sdkCtx := sdk.UnwrapSDKContext(ctx)
+		evmKeeper := k.getEvmKeeper(0)
+		acc := evmKeeper.GetAccountWithoutBalance(sdkCtx, address)
+
+		if acc == nil || !acc.IsContract() {
+			return nil, errorsmod.Wrapf(types.ErrInvalidCallback, "callback address is not a contract")
+		}
 	}
 
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
--- warden/x/async/types/v1beta1/errors.go
+++ warden/x/async/types/v1beta1/errors.go
@@ -8,8 +8,9 @@ import (
 
 // x/async module sentinel errors
 var (
-	ErrInvalidSigner          = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrInvalidHandler         = sdkerrors.Register(ModuleName, 1102, "invalid handler name")
-	ErrInvalidFutureInput     = sdkerrors.Register(ModuleName, 1103, "invalid future input")
+	ErrInvalidSigner      = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidHandler     = sdkerrors.Register(ModuleName, 1102, "invalid handler name")
+	ErrInvalidFutureInput = sdkerrors.Register(ModuleName, 1103, "invalid future input")
 	ErrFutureAlreadyHasResult = sdkerrors.Register(ModuleName, 1104, "future already has result")
+	ErrInvalidCallback    = sdkerrors.Register(ModuleName, 1105, "invalid callback")
 )

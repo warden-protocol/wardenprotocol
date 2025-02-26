6042dc212c5ae6575722990579b7687851020b5a
Andrei Beliaev <andrei.beliaev@eqlab.online>
2025-02-26 14:09:12 +0500

    Rename timeout -> timeout_sec

--- prophet/handlers/http/config/config.go
+++ prophet/handlers/http/config/config.go
@@ -4,17 +4,17 @@ package config
 // be set in the app.toml file.
 
 type Config struct {
-	Enabled bool     `mapstructure:"enabled" toml:"enabled"`
-	URLs    []string `mapstructure:"urls" toml:"urls"`
-	Timeout int      `mapstructure:"timeout" toml:"timeout"`
+	Enabled    bool     `mapstructure:"enabled" toml:"enabled"`
+	URLs       []string `mapstructure:"urls" toml:"urls"`
+	TimeoutSec int      `mapstructure:"timeout_sec" toml:"timeout_sec"`
 }
 
 // DefaultConfig returns a default configuration for http.
 func DefaultConfig() *Config {
 	return &Config{
-		Enabled: true,
-		URLs:    []string{"https://api.coingecko.com"},
-		Timeout: 5,
+		Enabled:    true,
+		URLs:       []string{"https://api.coingecko.com"},
+		TimeoutSec: 5,
 	}
 }
 
@@ -31,5 +31,5 @@ enabled = "{{ .Http.Enabled }}"
 urls = ["{{ range $i, $url := .Http.URLs }}{{if $i}},{{end}}{{$url}}{{end}}"]
 
 # Timeout in seconds for the HTTP client
-timeout = {{ .Http.Timeout }}
+timeout_sec = {{ .Http.TimeoutSec }}
 `

41728517e65d10b4b352e20caa4b0e447116bbc4
Maks Nabokov <max.nab@equilibrium.io>
2025-04-02 14:24:12 +0300

    add v1 types

--- automated-orders/packages/blockchain-library/src/types/order/functions.ts
+++ automated-orders/packages/blockchain-library/src/types/order/functions.ts
@@ -1,12 +1,4 @@
-import { AbiFunction, Hex } from 'viem';
-
-export interface IExecutionData {
-  caller: Hex;
-  to: Hex;
-  chainId: bigint;
-  data: Hex;
-  value: bigint;
-}
+import { AbiFunction } from 'viem';
 
 export const CanExecuteOrderAbi: AbiFunction = {
   type: 'function',
@@ -34,85 +26,4 @@ export const IsExecutedOrderAbi: AbiFunction = {
     },
   ],
   stateMutability: 'view',
-};
-
-export const ExecutionDataAbi: AbiFunction = {
-  type: 'function',
-  name: 'executionData',
-  inputs: [],
-  outputs: [
-    {
-      name: 'executionData',
-      type: 'tuple',
-      internalType: 'struct ExecutionData',
-      components: [
-        {
-          name: 'caller',
-          type: 'address',
-          internalType: 'address',
-        },
-        {
-          name: 'to',
-          type: 'address',
-          internalType: 'address',
-        },
-        {
-          name: 'chainId',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
-        {
-          name: 'data',
-          type: 'bytes',
-          internalType: 'bytes',
-        },
-        {
-          name: 'value',
-          type: 'uint256',
-          internalType: 'uint256',
-        },
-      ],
-    },
-  ],
-  stateMutability: 'view',
-};
-
-export const ExecuteAbi: AbiFunction = {
-  type: 'function',
-  name: 'execute',
-  inputs: [
-    {
-      name: 'nonce',
-      type: 'uint256',
-      internalType: 'uint256',
-    },
-    {
-      name: 'gas',
-      type: 'uint256',
-      internalType: 'uint256',
-    },
-    {
-      name: 'gasPrice',
-      type: 'uint256',
-      internalType: 'uint256',
-    },
-    {
-      name: 'maxPriorityFeePerGas',
-      type: 'uint256',
-      internalType: 'uint256',
-    },
-    {
-      name: 'maxFeePerGas',
-      type: 'uint256',
-      internalType: 'uint256',
-    },
-  ],
-  outputs: [
-    {
-      name: '',
-      type: 'bool',
-      internalType: 'bool',
-    },
-  ],
-  stateMutability: 'nonpayable',
 };

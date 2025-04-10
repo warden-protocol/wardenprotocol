b5a9877369e45f0e14c27ac1162a6baa85a3d54e
Antonio Pitasi <antonio@pitasi.dev>
2025-04-10 14:03:24 +0200

    feat(x/async): add plugins db collection

--- warden/x/async/keeper/genesis.go
+++ warden/x/async/keeper/genesis.go
@@ -16,6 +16,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/warden-protocol/wardenprotocol/prophet"
 	types "github.com/warden-protocol/wardenprotocol/warden/x/async/types/v1beta1"
 )
 
@@ -24,5 +25,19 @@ func (k *Keeper) ImportState(ctx sdk.Context, genState types.GenesisState) error
 }
 
 func (k *Keeper) ExportState(ctx sdk.Context, genState *types.GenesisState) error {
+	return nil
+}
+
+func (k *Keeper) AddProphetPlugins(ctx sdk.Context) error {
+	for _, p := range prophet.RegisteredPlugins() {
+		if err := k.AddPlugin(ctx, types.Plugin{
+			Id:          p,
+			Creator:     "",
+			Description: "",
+		}); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }

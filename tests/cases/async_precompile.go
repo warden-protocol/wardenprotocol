fb4753af24d2fb6b8559d9670d061bc1bfee83d4
Maks Nabokov <max.nab@equilibrium.io>
2025-04-18 18:07:23 +0300

    fix test

--- tests/cases/async_precompile.go
+++ tests/cases/async_precompile.go
@@ -50,7 +50,8 @@ func (c *Test_AsyncPrecompile) Run(t *testing.T, _ framework.BuildResult) {
 		"echo",
 		[]byte("USDT"),
 		maxFee,
-		common.HexToAddress("0x0000000000000000000000000000000000000000"))
+		common.HexToAddress("0x0000000000000000000000000000000000000000"),
+		async.TypesCallbackParams{})
 	require.NoError(t, err)
 
 	addTaskReceipt, err := bind.WaitMined(t.Context(), evmClient, addTaskTx)

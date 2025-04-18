4366a50e43d590c91c58530fabcd2847df0578a0
Maks Nabokov <max.nab@equilibrium.io>
2025-04-18 18:06:58 +0300

    sched precompile

--- warden/x/sched/keeper/query.go
+++ warden/x/sched/keeper/query.go
@@ -4,4 +4,10 @@ import (
 	types "github.com/warden-protocol/wardenprotocol/warden/x/sched/types/v1beta1"
 )
 
+// NewQueryServerImpl returns an implementation of the MsgServer interface
+// for the provided Keeper.
+func NewQueryServerImpl(keeper Keeper) types.QueryServer {
+	return &keeper
+}
+
 var _ types.QueryServer = Keeper{}

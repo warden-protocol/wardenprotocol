993488f9c393a76a3aef0e5e4b925164c99a8ca8
Maks Nabokov <max.nab@equilibrium.io>
2025-05-30 11:38:14 +0300

    fix

--- solidity/hyperlane/scripts/DeployHook.s.sol
+++ solidity/hyperlane/scripts/DeployHook.s.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 import "forge-std/src/Script.sol";
 import "forge-std/src/console.sol";
 import { StaticAggregationHookFactory } from "@hyperlane-xyz/contracts/hooks/aggregation/StaticAggregationHookFactory.sol";
+import { StaticAggregationHook } from "@hyperlane-xyz/contracts/hooks/aggregation/StaticAggregationHook.sol";
 import { TypeCasts } from "@hyperlane-xyz/contracts/libs/TypeCasts.sol";
 import { IMailbox } from "@hyperlane-xyz/contracts/interfaces/IMailbox.sol";
+import { IGasOracle } from "@hyperlane-xyz/contracts/interfaces/IGasOracle.sol";
 import { InterchainGasPaymaster } from "@hyperlane-xyz/contracts/hooks/igp/InterchainGasPaymaster.sol";
 import { ERC5164PayableHook } from "../src/ERC5164PayableHook.sol";
 
@@ -28,10 +30,10 @@ contract SendMessageScript is Script {
         );
 
         InterchainGasPaymaster igp = InterchainGasPaymaster(0x69982FD5372A86D66614A55f97Dd71Ac30135968); // IGP hook
-        destinationIsm = 0x4043B5bd20Cb3a98DC0ECD3a73769fE0580f0232; // address of destination ERC5164Ism
+        address destinationIsm = 0x4043B5bd20Cb3a98DC0ECD3a73769fE0580f0232; // address of destination ERC5164Ism
 
-        erc5164Owner = 0x6EA8AC1673402989E7B653AE4E83B54173719C30;
-        erc5164Beneficiary = 0x6EA8AC1673402989E7B653AE4E83B54173719C30;
+        address erc5164Owner = 0x6Ea8aC1673402989e7B653aE4e83b54173719C30;
+        address erc5164Beneficiary = 0x6Ea8aC1673402989e7B653aE4e83b54173719C30;
 
         address[] memory hooks = new address[](2);
         hooks[0] = address(igp); // IGP hook
@@ -42,19 +44,22 @@ contract SendMessageScript is Script {
             destinationIsm.addressToBytes32());
         
         erc5164PayableHook.initialize(erc5164Owner, erc5164Beneficiary);
-        InterchainGasPaymaster.DomainGasConfig memory domainGasConfig = igp.destinationGasConfigs(destinationDomain);
+        (IGasOracle gasOracle, uint96 gasOverhead) = igp.destinationGasConfigs(destinationDomain);
         InterchainGasPaymaster.GasParam memory gasParam = 
-            InterchainGasPaymaster.GasParam(destinationDomain, domainGasConfig);
+            InterchainGasPaymaster.GasParam(
+                destinationDomain,
+                InterchainGasPaymaster.DomainGasConfig(gasOracle, gasOverhead));
+
         InterchainGasPaymaster.GasParam[] memory gasParams = new InterchainGasPaymaster.GasParam[](1);
         gasParams[0] = gasParam;
-        erc5164PayableHook.setDestinationGasConfigs(_configs);
+        erc5164PayableHook.setDestinationGasConfigs(gasParams);
         hooks[1] = address(erc5164PayableHook);
-        hook = StaticAggregationHook(factory.deploy(hooks));
+        StaticAggregationHook hook = StaticAggregationHook(factory.deploy(hooks));
 
         console.log("Deployed ERC5164PayableHook hook:");
         console.logAddress(address(erc5164PayableHook));
         console.log("Deployed aggregation hook:");
-        console.logAddress(hook);
+        console.logAddress(address(hook));
 
         vm.stopBroadcast();
     }

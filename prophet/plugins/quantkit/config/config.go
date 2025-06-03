a9e401552d6ead1808c342df24e177dc6e41245c
Andrei <andrei@wardenprotcol.org>
2025-06-03 09:16:00 +0200

    fixed structure

---
+++ prophet/plugins/quantkit/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+type Config struct {
+	Enabled bool   `mapstructure:"enabled" toml:"enabled"`
+	ApiKey  string `mapstructure:"api-key" toml:"api-key"`
+	ApiURL  string `mapstructure:"api-url" toml:"api-url"`
+}
+
+// DefaultConfig returns a default configuration for http.
+func DefaultConfig() *Config {
+	return &Config{
+		Enabled: false,
+		ApiKey:  "",
+		ApiURL:  "",
+	}
+}
+
+const DefaultConfigTemplate = `
+###############################################################################
+###                      Quantkit configuration                               ###
+###############################################################################
+[quantkit]
+
+# Is QuantKit plugin enabled
+enabled = "{{ .Quantkit.Enabled }}"
+
+# API Key used when making Quantkit API requests
+api-key = "{{ .Quantkit.ApiKey }}"
+
+# API URL used when making Quantkit API requests
+api-url = "{{ .Quantkit.ApiURL }}"
+`

95725efccdfab548f343e43d66a2b212c5d68138
Maks Nabokov <max.nab@equilibrium.io>
2025-06-11 16:43:01 +0300

    add origin to callback example

--- solidity/hyperlane/src/HyperlaneCallback.sol
+++ solidity/hyperlane/src/HyperlaneCallback.sol
@@ -5,11 +5,67 @@ import "precompile-async/IAsync.sol";
 import "precompile-sched/ISched.sol";
 import "precompile-common/Types.sol";
 import { IMailbox } from "@hyperlane-xyz/contracts/interfaces/IMailbox.sol";
+import { IPostDispatchHook } from "@hyperlane-xyz/contracts/interfaces/hooks/IPostDispatchHook.sol";
 import { TypeCasts } from "@hyperlane-xyz/contracts/libs/TypeCasts.sol";
-import { IInterchainSecurityModule, ISpecifiesInterchainSecurityModule } from '@hyperlane-xyz/contracts/interfaces/IInterchainSecurityModule.sol';
+import { 
+    IInterchainSecurityModule,
+    ISpecifiesInterchainSecurityModule
+} from "@hyperlane-xyz/contracts/interfaces/IInterchainSecurityModule.sol";
 
 error Unauthorized();
 error NotReady();
+
+contract Origin {
+    IMailbox public mailbox;
+    bytes32 public destinationSender;
+    bytes public destinationMsg;
+    constructor(address _mailbox) {
+        mailbox = IMailbox(_mailbox);
+    }
+    function dispatch(
+        uint32 destinationDomain,
+        bytes32 recipientAddress,
+        bytes calldata messageBody
+    ) external payable returns (bytes32 messageId) {
+        return mailbox.dispatch{value: msg.value}(destinationDomain, recipientAddress, messageBody);
+    }
+    function quoteDispatch(
+        uint32 destinationDomain,
+        bytes32 recipientAddress,
+        bytes calldata messageBody
+    ) external view returns (uint256 fee) {
+        return mailbox.quoteDispatch(destinationDomain, recipientAddress, messageBody);
+    }
+
+    function dispatch(
+        uint32 destinationDomain,
+        bytes32 recipientAddress,
+        bytes calldata messageBody,
+        bytes calldata hookMetadata,
+        IPostDispatchHook hook
+    ) external payable returns (bytes32 messageId) {
+        return mailbox.dispatch{value: msg.value}(destinationDomain, recipientAddress, messageBody, hookMetadata, hook);
+    }
+
+    function quoteDispatch(
+        uint32 destinationDomain,
+        bytes32 recipientAddress,
+        bytes calldata messageBody,
+        bytes calldata hookMetadata,
+        IPostDispatchHook hook
+    ) external view returns (uint256 fee) {
+        return mailbox.quoteDispatch(destinationDomain, recipientAddress, messageBody, hookMetadata, hook);
+    }
+
+    function handle(
+        uint32,
+        bytes32 _sender,
+        bytes calldata _message
+    ) external payable {
+        destinationSender = _sender;
+        destinationMsg = _message;
+    }
+}
 
 contract Destination is ISpecifiesInterchainSecurityModule {
     using TypeCasts for address;

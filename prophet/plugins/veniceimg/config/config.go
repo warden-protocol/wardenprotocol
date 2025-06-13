6773e8bde59c0d51bf3b1555bc2468d26246e215
Margarita Skomorokh <ijonele@gmail.com>
2025-06-13 10:09:28 +0200

    Venice image gen (#1551)
    * created a directory for the new plugin
    
    * registered the plugin
    
    * added new logic for the plugin
    
    * fixed the order of imports
    
    * fixed formatting
    
    * fixed formatting
    
    * fixed formatting

---
+++ prophet/plugins/veniceimg/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+type Config struct {
+	Enabled bool   `mapstructure:"enabled" toml:"enabled"`
+	ApiKey  string `mapstructure:"api-key" toml:"api-key"`
+}
+
+// DefaultConfig returns a default configuration for veniceimg.
+func DefaultConfig() *Config {
+	return &Config{
+		Enabled: false,
+		ApiKey:  "",
+	}
+}
+
+const DefaultConfigTemplate = `
+###############################################################################
+###                       VeniceImg  configuration                          ###
+###############################################################################
+[veniceimg]
+
+# Is VeniceImg plugin enabled
+enabled = "{{ .Veniceimg.Enabled }}"
+
+# API Key used when making VeniceImg API requests
+api-key = "{{ .Veniceimg.ApiKey }}"
+`

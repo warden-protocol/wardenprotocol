278e9c2c9774a6656c5938361937034a53de6338
Aleksandr Tretiakov <backsapce@hotmail.com>
2025-07-02 16:34:54 +0300

    Code review

--- warden/x/async/types/v1beta1/errors.go
+++ warden/x/async/types/v1beta1/errors.go
@@ -28,8 +28,4 @@ var (
 	ErrInvalidCallback      = sdkerrors.Register(ModuleName, 1105, "invalid callback")
 	ErrInsufficientFees     = sdkerrors.Register(ModuleName, 1106, "insufficient fees for plugin")
 	ErrNoSolvers            = sdkerrors.Register(ModuleName, 1107, "no solvers")
-	ErrInvalidTaskId        = sdkerrors.Register(ModuleName, 1108, "invalid task id")
-	ErrInvalidPluginId      = sdkerrors.Register(ModuleName, 1109, "invalid plugin id")
-	ErrTaskAlreadyHasScore  = sdkerrors.Register(ModuleName, 1110, "task already has score")
-	ErrUnauthorised         = sdkerrors.Register(ModuleName, 1111, "unauthorized action")
 )

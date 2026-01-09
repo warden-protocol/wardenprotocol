cd6ab3e73713f726c8bf6581eddb37852a136e40
Antonio Pitasi <antonio@pitasi.dev>
2026-01-09 14:44:39 +0100

    feat: register erc20 mapping to native token

--- warden/app/upgrades.go
+++ warden/app/upgrades.go
@@ -7,39 +7,35 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/cosmos/evm/x/erc20/types"
 	evmtypes "github.com/cosmos/evm/x/vm/types"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func (app App) RegisterUpgradeHandlers() {
-	app.UpgradeKeeper.SetUpgradeHandler("v0.7.2", func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+	evmtypes.SetDefaultEvmCoinInfo(evmtypes.EvmCoinInfo{
+		Denom:         "award",
+		ExtendedDenom: "award",
+		DisplayDenom:  "WARD",
+		Decimals:      18,
+	})
+
+	app.UpgradeKeeper.SetUpgradeHandler("v0.7.4", func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
 
-		// cosmos/evm requires denom metadata to be set starting from v0.5.
-		// Set metadata, if not already present.
-		denom := "award"
-		if _, metadataFound := app.BankKeeper.GetDenomMetaData(ctx, denom); !metadataFound {
-			app.BankKeeper.SetDenomMetaData(ctx, banktypes.Metadata{
-				Description: "The native token for Warden Protocol.",
-				DenomUnits: []*banktypes.DenomUnit{
-					{Denom: denom},
-					{Denom: "ward", Exponent: 18},
-				},
-				Base:    denom,
-				Display: "ward",
-				Name:    "WARD",
-				Symbol:  "WARD",
-			})
+		nativeErc20Address := "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+		pair := types.NewTokenPair(
+			common.HexToAddress(nativeErc20Address),
+			"award",
+			types.OWNER_MODULE,
+		)
+
+		if err := app.Erc20Keeper.SetToken(sdkCtx, pair); err != nil {
+			return nil, fmt.Errorf("erc20 set token: %w", err)
 		}
 
-		// Initialize EvmCoinInfo in the module store.
-		if err := app.EVMKeeper.InitEvmCoinInfo(sdkCtx); err != nil {
-			return nil, err
-		}
-
-		// Enable default preinstalls.
-		if err := app.EVMKeeper.AddPreinstalls(sdkCtx, evmtypes.DefaultPreinstalls); err != nil { //nolint
-			return nil, fmt.Errorf("enable preinstalls: %w", err)
+		if err := app.Erc20Keeper.EnableNativePrecompile(sdkCtx, common.HexToAddress(nativeErc20Address)); err != nil {
+			return nil, fmt.Errorf("erc20 enable native precompile: %w", err)
 		}
 
 		return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
